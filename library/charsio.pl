e9ac4e269ebb410d54c7a5afd1caff10ff42cc15
Andrew Davison <andrew.davison@gmail.com>
2022-09-06 16:49:20 +1000

    WIP

--- library/charsio.pl
+++ library/charsio.pl
@@ -1,20 +1,31 @@
-:- module(charsio, [ fabricate_var_name/3 ]).
+:- module(charsio, [
+		read_line_to_chars/3,
+		get_n_chars/3,
+		fabricate_var_name/3
+	]).
+
+read_line_to_chars(Stream, Cs0, Cs) :-
+	getline(Stream, Line,[terminator(true)]),
+	partial_string(Line,Cs0,Cs).
+
+get_n_chars(Stream, N, Cs) :-
+	bread(Stream, N, Cs).
 
 fabricate_var_name(VarType, VarName, N) :-
-    char_code('A', AC),
-    LN is N mod 26 + AC,
-    char_code(LC, LN),
-    NN is N // 26,
-    (  NN =:= 0 ->
-       (  VarType == fabricated ->
-          atom_chars(VarName, ['_', LC])
-       ;  VarType == numbervars ->
-          atom_chars(VarName, [LC])
-       )
-    ;  number_chars(NN, NNChars),
-       (  VarType == fabricated ->
-          atom_chars(VarName, ['_', LC | NNChars])
-       ;  VarType == numbervars ->
-          atom_chars(VarName, [LC | NNChars])
-       )
-    ).
+	char_code('A', AC),
+	LN is N mod 26 + AC,
+	char_code(LC, LN),
+	NN is N // 26,
+	(  NN =:= 0 ->
+		(  VarType == fabricated ->
+		  atom_chars(VarName, ['_', LC])
+		;  VarType == numbervars ->
+		  atom_chars(VarName, [LC])
+		)
+	;  number_chars(NN, NNChars),
+		(  VarType == fabricated ->
+		  atom_chars(VarName, ['_', LC | NNChars])
+		;  VarType == numbervars ->
+		  atom_chars(VarName, [LC | NNChars])
+		)
+	).
