8fe3821ef449578cd423d683d663cef09355b4bd
Andrew Davison <andrew.davison@gmail.com>
2022-09-29 08:07:39 +1000

    Trying to use DBL_DECIMAL_DIG (full 18 digits of precision) in printing, re issue #49

--- src/print.c
+++ src/print.c
@@ -724,7 +724,7 @@ ssize_t print_term_to_buf(query *q, char *dst, size_t dstlen, cell *c, pl_idx_t 
 
 	if (is_float(c)) {
 		char tmpbuf[256];
-		sprintf(tmpbuf, "%.*g", DBL_DECIMAL_DIG-(q->ignore_ops?0:1), get_float(c));
+		sprintf(tmpbuf, "%.*g", DBL_DECIMAL_DIG, get_float(c));
 		reformat_float(tmpbuf);
 		dst += snprintf(dst, dstlen, "%s", tmpbuf);
 		q->last_thing_was_symbol = false;
