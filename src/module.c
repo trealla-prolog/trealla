20f47de9fc845817cf36fddd0713a98fa0c57e0c
Andrew Davison <andrew.davison@gmail.com>
2022-10-24 10:54:03 +1000

    WIP

--- src/module.c
+++ src/module.c
@@ -42,7 +42,6 @@ static const op_table g_ops[] =
 	{"module", OP_FX, 1150},
 	{"use_module", OP_FX, 1150},
 	{"ensure_loaded", OP_FX, 1150},
-
 	{"meta_predicate", OP_FX, 1150},
 
 	{"\\+", OP_FY, 900},
