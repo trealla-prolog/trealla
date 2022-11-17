8d003be880ebae79bab56de122f3ae9c9ba20aa1
Andrew Davison <andrew.davison@gmail.com>
2022-11-17 22:55:17 +1000

    Work on stream aliases

--- src/print.c
+++ src/print.c
@@ -672,6 +672,14 @@ ssize_t print_term_to_buf(query *q, char *dst, size_t dstlen, cell *c, pl_idx_t 
 		return dst - save_dst;
 	}
 
+	if ((c->tag == TAG_INTEGER) && (c->flags & FLAG_INT_STREAM)) {
+		int n = get_stream(q, c);
+		dst += snprintf(dst, dstlen, "'<$stream>'(%p)", c);
+		q->last_thing_was_symbol = false;
+		q->was_space = false;
+		return dst - save_dst;
+	}
+
 	if (q->is_dump_vars && is_blob(c)) {
 		dst += snprintf(dst, dstlen, "<$blob>(0x%X)", (int)get_smallint(c));
 		q->last_thing_was_symbol = false;
