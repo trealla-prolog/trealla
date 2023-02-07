927f13fdb0b06117e32800c534fd42e0a95eb078
Andrew Davison <andrew.davison@gmail.com>
2023-02-07 13:14:43 +1000

    More on help

--- library/sqlite3.pl
+++ library/sqlite3.pl
@@ -7,8 +7,8 @@
 % WARNING: This is experimental!
 
 sqlite3_query(Conn, Sql, Cols, Names) :-
-	flag('SQLITE_OK', SQLITE_OK),
-	flag('SQLITE_ROW', SQLITE_ROW),
+	sqlite_flag('SQLITE_OK', SQLITE_OK),
+	sqlite_flag('SQLITE_ROW', SQLITE_ROW),
 	sqlite3_prepare_v2(Conn, Sql, -1, Statement, _, Ret), Ret =:= SQLITE_OK,
 	repeat,
 		(	( sqlite3_step(Statement, Ret2), Ret2 =:= SQLITE_ROW )
@@ -20,10 +20,10 @@ sqlite3_query(Conn, Sql, Cols, Names) :-
 
 get_col(_, 0, Cols, Cols, Names, Names) :- !.
 get_col(Statement, Count, Col, Cols, Name, Names) :-
-	flag('SQLITE_INTEGER', SQLITE_INTEGER),
-	flag('SQLITE_FLOAT', SQLITE_FLOAT),
-	flag('SQLITE_TEXT', SQLITE_TEXT),
-	flag('SQLITE_BLOB', SQLITE_BLOB),
+	sqlite_flag('SQLITE_INTEGER', SQLITE_INTEGER),
+	sqlite_flag('SQLITE_FLOAT', SQLITE_FLOAT),
+	sqlite_flag('SQLITE_TEXT', SQLITE_TEXT),
+	sqlite_flag('SQLITE_BLOB', SQLITE_BLOB),
 	Count1 is Count - 1,
 	sqlite3_column_name(Statement, Count1, NewName),
 	sqlite3_column_type(Statement, Count1, Type),
