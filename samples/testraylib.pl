00399da99dd895b863e4147d4c41b0238850cc9f
Andrew Davison <andrew.davison@gmail.com>
2023-02-19 13:47:40 +1000

    WIP

--- samples/testraylib.pl
+++ samples/testraylib.pl
@@ -12,9 +12,9 @@ loop :-
 	;	(
 		'BeginDrawing',
 		'ClearBackground'([color,245,245,245,255]),
-		'DrawText'("Congrats! You created your first window!",198,200,20,[color,200,200,200,255]),
+		'DrawText'("Congrats! You created your first window!", 198, 200, 20, [color,200,200,200,255]),
 		'EndDrawing',
-		delay(100),
+		delay(10),
 		loop
 		)
 	).
