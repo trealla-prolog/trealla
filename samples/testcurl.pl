05ac6e691eb12eda7ab3e573b04bbd4de9950b84
Andrew Davison <andrew.davison@gmail.com>
2023-03-02 19:38:32 +1000

    Add module library(curl)

--- samples/testcurl.pl
+++ samples/testcurl.pl
@@ -1,6 +1,6 @@
 :- use_module(library(curl)).
 
-get(Url, Filename) :-
+cget(Url, Filename) :-
 	curlopt(xCURLOPT_WRITEDATA, _, CURLOPT_WRITEDATA),
 	curlopt(xCURLOPT_URL, _, CURLOPT_URL),
 
@@ -13,6 +13,30 @@ get(Url, Filename) :-
 			stream_property(S, file(F)),
 			curl_easy_setopt(Curl, CURLOPT_WRITEDATA, F, _),
 			curl_easy_setopt(Curl, CURLOPT_URL, Url, _),
+			curl_easy_perform(Curl, Res),
+			Res =:= 0
+		),
+		(
+			close(S)
+		)
+	),
+	curl_easy_cleanup(Curl).
+
+cput(Url, Filename) :-
+	curlopt(xCURLOPT_READDATA, _, CURLOPT_READDATA),
+	curlopt(xCURLOPT_URL, _, CURLOPT_URL),
+	curlopt(xCURLOPT_PUT, _, CURLOPT_PUT),
+
+	curl_easy_init(Curl),
+	setup_call_cleanup(
+		(
+			open(Filename, read, S, [])
+		),
+		(
+			stream_property(S, file(F)),
+			curl_easy_setopt(Curl, CURLOPT_READDATA, F, _),
+			curl_easy_setopt(Curl, CURLOPT_URL, Url, _),
+			curl_easy_setopt(Curl, CURLOPT_PUT, Url, _),
 			curl_easy_perform(Curl, Res),
 			Res =:= 0
 		),
