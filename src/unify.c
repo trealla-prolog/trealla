a27730b5a045804c3bfee16e5ffea993ab679c07
Andrew Davison <andrew.davison@gmail.com>
2023-03-12 22:01:49 +1000

    Make compare/3 efficient

--- src/unify.c
+++ src/unify.c
@@ -11,6 +11,7 @@ static int compare_lists(query *q, cell *p1, pl_idx_t p1_ctx, cell *p2, pl_idx_t
 {
 	LIST_HANDLER(p1);
 	LIST_HANDLER(p2);
+	q->mgen++;
 
 	while (is_iso_list(p1) && is_iso_list(p2)) {
 		cell *h1 = LIST_HEAD(p1);
@@ -39,19 +40,47 @@ static int compare_lists(query *q, cell *p1, pl_idx_t p1_ctx, cell *p2, pl_idx_t
 				e2->mgen = q->mgen;
 		}
 
-		h1 = deref(q, h1, p1_ctx);
-		pl_idx_t h1_ctx = q->latest_ctx;
-		h2 = deref(q, h2, p2_ctx);
-		pl_idx_t h2_ctx = q->latest_ctx;
-
 		if (!both) {
+			h1 = deref(q, h1, p1_ctx);
+			pl_idx_t h1_ctx = q->latest_ctx;
+			h2 = deref(q, h2, p2_ctx);
+			pl_idx_t h2_ctx = q->latest_ctx;
+			uint64_t save_mgen = q->mgen;
 			int val = compare_internal(q, h1, h1_ctx, h2, h2_ctx, depth+1);
 			if (val) return val;
+			q->mgen = save_mgen;
 		} else
 			return 0;
 
+		if (e1) e1->mgen = 0;
+		if (e2) e2->mgen = 0;
+
 		p1 = LIST_TAIL(p1);
 		p2 = LIST_TAIL(p2);
+		both = 0;
+
+		if (is_var(p1)) {
+			const frame *f1 = GET_FRAME(p1_ctx);
+			e1 = GET_SLOT(f1, p1->var_nbr);
+
+			if (e1->mgen == q->mgen)
+				both++;
+			else
+				e1->mgen = q->mgen;
+		}
+
+		if (is_var(p2)) {
+			const frame *f2 = GET_FRAME(p2_ctx);
+			e2 = GET_SLOT(f2, p2->var_nbr);
+
+			if (e2->mgen == q->mgen)
+				both++;
+			else
+				e2->mgen = q->mgen;
+		}
+
+		if (both)
+			return 0;
 
 		p1 = deref(q, p1, p1_ctx);
 		p1_ctx = q->latest_ctx;
@@ -1006,7 +1035,7 @@ static bool unify_lists(query *q, cell *p1, pl_idx_t p1_ctx, cell *p2, pl_idx_t 
 		pl_idx_t h1_ctx = q->latest_ctx;
 		h2 = deref(q, h2, p2_ctx);
 		pl_idx_t h2_ctx = q->latest_ctx;
-		uint32_t save_mgen = q->mgen;
+		uint64_t save_mgen = q->mgen;
 
 		if (!unify_internal(q, h1, h1_ctx, h2, h2_ctx, depth+1))
 			return false;
