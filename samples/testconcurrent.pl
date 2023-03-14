8c28b66d3f4fefcf5ed6e126bd489bd122afa99c
Andrew Davison <andrew.davison@gmail.com>
2023-03-15 09:32:33 +1000

    WIP

--- samples/testconcurrent.pl
+++ samples/testconcurrent.pl
@@ -7,7 +7,7 @@ test1(C) :-
     await(F, [A,B]),
     C is 2 * A + B.
 
-test(C) :-
+test2(C) :-
     future(X, slow_predicate(X), F1),
     future(Y, other_slow_thing(Y), F2),
     await(F1, A),
@@ -21,7 +21,23 @@ test3(C) :-
     await(F, V),
     C = V.
 
-
 slow_predicate(X) :- delay(900), X = 41.
 other_slow_thing(X) :- delay(500), X = 1.
+
+
+:- use_module(library(format)).
+:- use_module(library(http)).
+:- use_module(library(pio)).
+
+test4(C) :-
+	future(Status1, geturl("www.google.com", Status1), F1),
+	future(Status2, geturl("www.bing.com", Status2), F2),
+	future(Status3, geturl("www.duckduckgo.com", Status3), F3),
+    future_all([F1,F2,F3], F),
+    await(F, StatusCodes),
+    C = StatusCodes.
+
+geturl(Url,Code) :-
+	http_get(Url,_Data,[status_code(Code),final_url(Location)]), !,
+	format("Job [~w] ~w ==> ~w done~n",[Url,Code,Location]).
 
