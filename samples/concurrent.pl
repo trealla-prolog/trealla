d2dd2155fb4efcad97af9ef0e91a63e438b32076
Andrew Davison <andrew.davison@gmail.com>
2023-03-15 08:36:05 +1000

    Add future_any/2 to samples/concurrent.pl

--- samples/concurrent.pl
+++ samples/concurrent.pl
@@ -15,22 +15,30 @@ future(X, Goal, F) :-
 	retract('$concurrent_count'(N)),
 	N1 is N + 1,
 	assertz('$concurrent_count'(N1)),
-	F = N1,
+	F = N,
 	Goal2 = [F,X,Goal],
-	Task0 = (Goal2=[F,X2,GoalX],call(GoalX),send([F-X2])),
+	Task0 = (Goal2=[F,X2,GoalX], call(GoalX), send([F-X2])),
 	copy_term(Task0, Task),
 	task(Task).
 
-future_all(Fs, Fs).
+future_all(Fs, all(Fs)).
+future_any(Fs, any(Fs)).
 
-await(Fs, Vars) :-
-	is_list(Fs),
+await(all(Fs), Vars) :-
 	!,
 	wait,
-	findall([F-Msg], (member(F,Fs), recv([F-Msg])), L0),
+	findall([F-Msg], (member(F, Fs), recv([F-Msg])), L0),
 	msort(L0, Msgs),
 	strip_prefix_(Msgs, [], L),
 	L = Vars.
+
+await(any(Fs), Var) :-
+	!,
+	wait,
+	recv([F-Msg]),
+	member(F, Fs),
+	!,
+	Var = Msg.
 
 await(F, Var) :-
 	repeat,
--- samples/testconcurrent.pl
+++ samples/testconcurrent.pl
@@ -7,12 +7,20 @@ test1(C) :-
     await(F, [A,B]),
     C is 2 * A + B.
 
-test2(C) :-
+test(C) :-
     future(X, slow_predicate(X), F1),
     future(Y, other_slow_thing(Y), F2),
     await(F1, A),
     await(F2, B),
     C is 2 * A + B.
+
+test3(C) :-
+    future(X, slow_predicate(X), F1),
+    future(Y, other_slow_thing(Y), F2),
+    future_any([F1,F2], F),
+    await(F, V),
+    C = V.
+
 
 slow_predicate(X) :- delay(900), X = 41.
 other_slow_thing(X) :- delay(500), X = 1.
