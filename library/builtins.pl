b70e80415b6f2c9664a02cd8a990e17697d89ffa
Andrew Davison <andrew.davison@gmail.com>
2023-03-23 11:19:31 +1000

    WIP

--- library/builtins.pl
+++ library/builtins.pl
@@ -55,7 +55,7 @@ catch(G, E, C) :-
 :- help(catch(:callable,+term,:callable), [iso(true)]).
 
 call_cleanup(G, C) :-
-	(var(C) -> throw(error(instantiation_error, setup_call_cleanup/3)) ; true),
+	(var(C) -> throw(error(instantiation_error, call_cleanup/3)) ; true),
 	'$register_cleanup'(ignore(C)),
 	'$call_cleanup'(
 		call(G),
@@ -1003,7 +1003,7 @@ dump_attvars :-
 %:- help(dump_attvars, [iso(false)]).
 
 call_residue_vars(Goal, Atts) :-
-	call(Goal),
+	Goal,
 	term_attvars(Goal, Atts).
 
 :- help(call_residue_vars(:callable, -list), [iso(false),desc('Find residual attributed variables left by Goal. This predicate is intended for reasoning about and debugging programs that use coroutining or constraints. To see why this predicate is necessary, consider a predicate that poses contradicting constraints on a variable, and where that variable does not appear in any argument of the predicate and hence does not yield any residual goals on the toplevel when the predicate is invoked. Such programs should fail, but sometimes succeed because the constraint solver is too weak to detect the contradiction. Ideally, delayed goals and constraints are all executed at the end of the computation. The meta predicate call_residue_vars/2 finds variables that are given attributes or whose attributes are modified by Goal, regardless of whether or not these variables are reachable from the arguments of Goal.')]).
