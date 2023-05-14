c9d521631fe0ec44918232c191a58b1283b38561
Andrew Davison <andrew.davison@gmail.com>
2023-05-14 16:59:09 +1000

    Improve 1st arg indexing look-ahead, re issue #167

--- src/query.c
+++ src/query.c
@@ -354,14 +354,24 @@ bool has_next_key(query *q)
 	if (!next)
 		return false;
 
+	if (next->next || !q->st.arg1_is_ground)
+		return true;
+
 	// Attempt look-ahead on 1st arg...
 
 	cl = &next->cl;
-	const cell *arg1 = cl->cells + 1;
+	cell *darg1 = cl->cells->val_off == g_neck_s ? cl->cells + 1+1 : cl->cells + 1;
 
-	if (q->st.arg1_is_ground && !next->next && q->pl->opt
-		&& (q->st.key->arity == 1) && is_atomic(arg1)) {
-		if (compare(q, q->st.key, q->st.curr_frame, cl->cells, q->st.curr_frame)) {
+	if (is_var(darg1))
+		return true;
+
+	cell *karg1 = deref(q, q->st.key + 1, q->st.curr_frame);
+
+	//DUMP_TERM("key", q->st.key, q->st.curr_frame, 1);
+	//DUMP_TERM("next", cl->cells, q->st.curr_frame, 0);
+
+	if (q->pl->opt && is_atomic(karg1)) {
+		if (compare(q, karg1, q->st.curr_frame, darg1, q->st.curr_frame)) {
 			return false;
 		}
 	}
