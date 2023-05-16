37e69170a8b4f770fb755010f57c999dafb14c47
Andrew Davison <andrew.davison@gmail.com>
2023-05-16 12:27:23 +1000

    Add library/gensym.pl

--- library/gensym.pl
+++ library/gensym.pl
@@ -1,7 +1,12 @@
 :- module(gensym, [gensym/2,
 		   reset_gensym/1]).
 
+:- use_module(library(error)).
+:- use_module(library(si)).
+
 gensym(Base, Unique) :-
+	must_be(var, Unique),
+	atom_si(Base),
 	( 'clause'('$gensym'(Base, N0), _)
 	-> (N is N0 + 1, retract('$gensym'(Base,N0)))
 	; N is 1
@@ -10,5 +15,6 @@ gensym(Base, Unique) :-
 	assertz('$gensym'(Base, N)).
 
 reset_gensym(Base) :-
+	atom_si(Base),
 	retract('$gensym'(Base, _)).
 
