87b344b869d5b4789991749a9fb943f58d92efba
Andrew Davison <andrew.davison@gmail.com>
2023-05-16 14:12:17 +1000

    WIP

--- library/gensym.pl
+++ library/gensym.pl
@@ -4,20 +4,20 @@
 :- use_module(library(si)).
 
 gensym_key(Base, BaseKey) :-
-    atom_concat('gensym_', Base, BaseKey).
+	atom_concat('gensym_', Base, BaseKey).
 
 gensym(Base, Unique) :-
 	must_be(var, Unique),
 	atom_si(Base),
-    gensym_key(Base, BaseKey),
+	gensym_key(Base, BaseKey),
 	( bb_get(BaseKey, UniqueID0)
 	-> N is UniqueID0 + 1
 	; N is 1
 	),
 	atomic_concat(Base, N, Unique),
-    bb_put(BaseKey, N).
+	bb_put(BaseKey, N).
 
 reset_gensym(Base) :-
 	atom_si(Base),
-    gensym_key(Base, BaseKey),
-    bb_put(BaseKey, 0).
+	gensym_key(Base, BaseKey),
+	bb_put(BaseKey, 0).
