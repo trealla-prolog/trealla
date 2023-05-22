052a26d7c7c9fcd59fe2f60a373738aef11e17e1
Andrew Davison <andrew.davison@gmail.com>
2023-05-22 17:19:04 +1000

    Fix leak, re issue #170, re issue #169

--- src/query.c
+++ src/query.c
@@ -728,6 +728,9 @@ static void trim_trail(query *q)
 		if (tr->var_ctx != q->st.curr_frame)
 			break;
 
+		const frame *f = GET_FRAME(tr->var_ctx);
+		slot *e = GET_SLOT(f, tr->var_nbr);
+		unshare_cell(&e->c);
 		q->st.tp--;
 	}
 }
