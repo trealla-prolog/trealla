d1860ecfd0fb8adc4374fa9815101cc02f43ef4b
Andrew Davison <andrew.davison@gmail.com>
2023-05-26 17:45:24 +1000

    Fix segfault, re issue #169

--- src/parser.c
+++ src/parser.c
@@ -3521,7 +3521,7 @@ bool run(parser *p, const char *pSrc, bool dump, query **subq, unsigned int yiel
 	}
 
 	SB(src);
-	SB_sprintf(src, "%s", pSrc);
+	SB_sprintf(src, "true,%s", pSrc);
 	SB_trim_ws(src);
 	SB_trim(src, '.');
 	SB_strcat(src, ".");
