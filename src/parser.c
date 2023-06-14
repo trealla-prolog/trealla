c88f0d9795d645a884f5040bc964cc2d17d177d1
Andrew Davison <andrew.davison@gmail.com>
2023-06-15 07:03:58 +1000

    Fix pl_query, re issue #181

--- src/parser.c
+++ src/parser.c
@@ -3543,7 +3543,7 @@ bool run(parser *p, const char *pSrc, bool dump, query **subq, unsigned int yiel
 	}
 
 	SB(src);
-	SB_sprintf(src, "true. %s", pSrc);
+	SB_sprintf(src, "%s", pSrc);
 	SB_trim_ws(src);
 	SB_trim(src, '.');
 	SB_strcat(src, ".");
