1c3b65514e84bbea3a6ba37b8cf90de7879c45d5
Andrew Davison <andrew.davison@gmail.com>
2023-06-16 16:57:20 +1000

    Fix cyclic unification looping, re issue #179

--- src/unify.c
+++ src/unify.c
@@ -1105,7 +1105,8 @@ static bool unify_lists(query *q, cell *p1, pl_idx_t p1_ctx, cell *p2, pl_idx_t 
 			pl_idx_t h2_ctx = q->latest_ctx;
 			bool ok = unify_internal(q, h1, h1_ctx, h2, h2_ctx, depth+1);
 			if (!ok) return false;
-		}
+		} else
+			return true;
 
 		if (e1) e1->vgen = 0;
 		if (e2) e2->vgen2 = 0;
