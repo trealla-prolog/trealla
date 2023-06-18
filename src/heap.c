a5787c7ebad4eae4c28788175e41ddee8a5a3773
Andrew Davison <andrew.davison@gmail.com>
2023-06-18 12:00:40 +1000

    WIP

--- src/heap.c
+++ src/heap.c
@@ -517,13 +517,8 @@ static cell *deep_clone2_to_tmp(query *q, cell *p1, pl_idx_t p1_ctx, unsigned de
 		return tmp;
 
 	bool cyclic = false;
-	bool is_partial = false;
 
-	//if (q->flags.occurs_check && is_iso_list(p1)
-	//	&& !check_list(q, p1, p1_ctx, &is_partial, NULL))
-	//	is_partial = true;
-
-	if (!is_partial && is_iso_list(p1)) {
+	if (is_iso_list(p1)) {
 		LIST_HANDLER(p1);
 
 		while (is_iso_list(p1)) {
