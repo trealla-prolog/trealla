49f0c6430d5876c928b27751fcbc78d4eb3fa950
Andrew Davison <andrew.davison@gmail.com>
2023-06-23 20:12:54 +1000

    WIP

--- tests/tests/test092.pl
+++ tests/tests/test092.pl
@@ -1,7 +1,7 @@
 :-initialization(main).
 
 main :-
-	A=[[]|B],B=[[],[]|B],A=B,
-	A=[A|A],B=[A|B],
-	\+ unify_with_occurs_check(A,[[[]]|A]).
+	A1=[[]|B1],B1=[[],[]|B1],A1=B1,
+	A2=[A2|A2],B2=[A2|B2],
+	\+ unify_with_occurs_check(A3,[[[]]|A3]).
 
