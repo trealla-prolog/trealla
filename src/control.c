c540bf5c4ad27ab458395d878f96a119303b48ea
Andrew Davison <andrew.davison@gmail.com>
2023-06-30 09:28:38 +1000

    WIP

--- src/control.c
+++ src/control.c
@@ -699,6 +699,9 @@ bool find_exception_handler(query *q, char *ball)
 	else
 		fprintf(stdout, "  ");
 
+	if (q->run_init)
+		fprintf(stdout, "\rWarning: Initialization goal exception: ");
+
 	//if (!q->run_init/*!is_interned(e) || strcmp(C_STR(q, e), "error")*/)
 	//	fprintf(stdout, "throw(");
 
