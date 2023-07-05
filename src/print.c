6b54475070a9de2f8293b9d84084b016a732aff1
Andrew Davison <andrew.davison@gmail.com>
2023-07-05 15:14:32 +1000

    WIP

--- src/print.c
+++ src/print.c
@@ -933,7 +933,7 @@ ssize_t print_term_to_buf(query *q, char *dst, size_t dstlen, cell *c, pl_idx_t 
 			return dst - save_dst;
 		}
 
-		if (is_var(c) && q->variable_names) {
+		if (is_var(c) && !is_anon(c) && q->variable_names) {
 			cell *l = q->variable_names;
 			pl_idx_t l_ctx = q->variable_names_ctx;
 			LIST_HANDLER(l);
