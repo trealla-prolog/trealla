38ca5d4664a63c03adb56a8f0fcb8fd2cb1833b3
Andrew Davison <andrew.davison@gmail.com>
2023-07-10 18:13:56 +1000

    Fix write option max_depth checking, re issue #250

--- src/streams.c
+++ src/streams.c
@@ -2483,8 +2483,10 @@ bool parse_write_params(query *q, cell *c, pl_idx c_ctx, cell **vnames, pl_idx *
 			return false;
 		}
 
-		if (is_integer(c1) && (get_smallint(&c[1]) >= 1))
+		if (is_integer(c1) && (get_smallint(&c[1]) >= 0))
 			q->max_depth = get_smallint(&c[1]);
+		else
+			throw_error(q, c, c_ctx, "domain_error", "write_option");
 	} else if (!CMP_STR_TO_CSTR(q, c, "fullstop")) {
 		if (is_var(c1)) {
 			throw_error(q, c1, c_ctx, "instantiation_error", "write_option");
