e6605df564d1a7c838826afc6bc3f5f39f4b70ab
Andrew Davison <andrew.davison@gmail.com>
2023-07-21 15:21:12 +1000

    WIP

--- src/control.c
+++ src/control.c
@@ -201,7 +201,7 @@ bool fn_iso_call_1(query *q)
 	check_heap_error(tmp2);
 	bool found = false;
 
-	if (!tmp2->match) {
+	if (!tmp2->match && 0) {
 		if ((tmp2->match = search_predicate(q->st.m, tmp2, NULL)) != NULL) {
 			tmp2->flags &= ~FLAG_BUILTIN;
 		} else if ((tmp2->fn_ptr = get_builtin_term(q->st.m, tmp2, &found, NULL)), found) {
