ca6c91918d4d9c737adc2fb1fba77944ee1ce882
Andrew Davison <andrew.davison@gmail.com>
2023-07-26 19:36:18 +1000

    Continue some code rationalization

--- src/query.c
+++ src/query.c
@@ -423,7 +423,7 @@ static bool find_key(query *q, predicate *pr, cell *key, pl_idx key_ctx)
 
 		cell *arg2 = NEXT_ARG(arg1);
 
-		if (is_var(arg2)) {
+		if (is_var(arg2) || pr->is_var_in_second_arg) {
 			q->st.curr_dbe = pr->head;
 			return true;
 		}
