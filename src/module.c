d83b43de8c943af09d32eb549c51c7ab379d567b
Andrew Davison <andrew.davison@gmail.com>
2023-07-26 19:11:36 +1000

    Continue some code rationalization

--- src/module.c
+++ src/module.c
@@ -1395,7 +1395,9 @@ static void assert_commit(module *m, db_entry *dbe, predicate *pr, bool append)
 		return;
 
 	if (!pr->idx) {
-		if (pr->cnt < 1500)
+		unsigned INDEX_THESHHOLD = pr->is_dynamic && pr->is_multifile ? 1500 : 500;
+
+		if (pr->cnt < INDEX_THESHHOLD)
 			return;
 
 		pr->idx = map_create(index_cmpkey, NULL, m);
@@ -1417,8 +1419,8 @@ static void assert_commit(module *m, db_entry *dbe, predicate *pr, bool append)
 			map_app(pr->idx, c, cl2);
 
 			if (pr->idx2) {
-				cell *arg1 = FIRST_ARG(c);
-				cell *arg2 = NEXT_ARG(arg1);
+				cell *arg1 = c + 1;
+				cell *arg2 = arg1 + arg1->nbr_cells;
 				map_app(pr->idx2, arg2, cl2);
 			}
 		}
@@ -1427,8 +1429,11 @@ static void assert_commit(module *m, db_entry *dbe, predicate *pr, bool append)
 	}
 
 	cell *c = get_head(dbe->cl.cells);
-	cell *arg1 = c->arity ? FIRST_ARG(c) : NULL;
-	cell *arg2 = c->arity > 1 ? NEXT_ARG(arg1) : NULL;
+	cell *arg1 = c->arity ? c + 1 : NULL;
+	cell *arg2 = arg1 ? arg1 + arg1->nbr_cells : NULL;
+
+	if (arg1 && is_var(arg1))
+		pr->is_var_in_first_arg = true;
 
 	if (!append) {
 		map_set(pr->idx, c, dbe);
