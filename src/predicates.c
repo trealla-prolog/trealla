da8e7cb96a3a088cac565a3f0dd934d5a5978132
Andrew Davison <andrew.davison@gmail.com>
2023-07-31 10:02:25 +1000

    WIP

--- src/predicates.c
+++ src/predicates.c
@@ -329,8 +329,6 @@ static bool fn_iso_findall_3(query *q)
 			p0 = deep_copy_to_heap(q, q->st.curr_cell, q->st.curr_frame, false);
 			check_heap_error(p0);
 			unify(q, q->st.curr_cell, q->st.curr_frame, p0, q->st.curr_frame);
-		} else if (is_var(xp1)) {
-			p0 = q->st.curr_cell;
 		} else
 			p0 = q->st.curr_cell;
 
