c5c95143a47daa383f89f6b4eb0ab66f779e63f8
Andrew Davison <andrew.davison@gmail.com>
2023-08-30 08:18:34 +1000

    WIP

--- tests/issues/test309.pl
+++ tests/issues/test309.pl
@@ -1,7 +1,7 @@
 :-initialization(main).
 
 main :-
-	\+ (A + B + C = (C*2) + (A*2) + (C*1*2), A=B, write(oops), nl), write(ok1), nl,
-	\+ (A1=B1*1, B1=B1*A1*A1, A1=B1, write(oops2), nl), write(ok2), nl, fail.
+	\+ (A + B + C = (C*2) + (A*2) + (C*1*2), A=B, write(oops), nl), write(ok), nl,
+	\+ (A1=B1*1, B1=B1*A1*A1, A1=B1, write(oops1), nl), write(ok1), nl, fail.
 main.
 
