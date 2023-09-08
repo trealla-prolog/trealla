dcd443f469110a241da3121ade7edcb763d3c471
Andrew Davison <andrew.davison@gmail.com>
2023-09-08 15:16:59 +1000

    Fix module atts hook-call to verify_attributes/3 to not bail early

---
+++ samples/nqueens.pl
@@ -0,0 +1,15 @@
+n_queens(N, Qs) :-
+        length(Qs, N),
+        Qs ins 1..N,
+        safe_queens(Qs).
+
+safe_queens([]).
+safe_queens([Q|Qs]) :- safe_queens(Qs, Q, 1), safe_queens(Qs).
+
+safe_queens([], _, _).
+safe_queens([Q|Qs], Q0, D0) :-
+        Q0 #\= Q,
+        abs(Q0 - Q) #\= D0,
+        D1 #= D0 + 1,
+        safe_queens(Qs, Q0, D1).
+
