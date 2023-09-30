04f243e9b295881f9bee5c685952f64c0f3ef495
Andrew Davison <andrew.davison@gmail.com>
2023-09-30 18:19:33 +1000

    Fix hook not processing all attvars, re issue #353

--- library/atts.pl
+++ library/atts.pl
@@ -31,6 +31,7 @@ process_var_([Att|Atts], Var, Val, SoFar, Goals) :-
 	attribute(M, F, A),
 	M:verify_attributes(Var, Val, NewGoals),
 	append(SoFar, NewGoals, MoreGoals),
+	!,
 	process_var_(Atts, Var, Val, MoreGoals, Goals).
 
 call_residue_vars(Goal, Atts) :-
