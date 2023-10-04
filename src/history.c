94bb05fba905af9e9573c49b1778cd2059796bb0
Andrew Davison <andrew.davison@gmail.com>
2023-10-05 08:45:03 +1000

    Fix extra space after tab completion, re issue #359

--- src/history.c
+++ src/history.c
@@ -184,6 +184,7 @@ static char *functor_name_generator(const char *text, int state)
 static char **functor_name_completion(const char *text, int start, int end)
 {
 	rl_attempted_completion_over = 1;
+	rl_completion_append_character = '\0';
 	return rl_completion_matches(text, functor_name_generator);
 }
 
