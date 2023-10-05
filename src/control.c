94b7ab294569d2ff3be629a5b5aee890a8f6a84e
Andrew Davison <andrew.davison@gmail.com>
2023-10-05 15:24:29 +1000

    WIP

--- src/control.c
+++ src/control.c
@@ -88,7 +88,7 @@ bool fn_iso_invoke_2(query *q)
 	check_heap_error(tmp);
 	pl_idx nbr_cells = PREFIX_LEN;
 
-	if (!is_builtin(p2) /*&& !tmp[nbr_cells].match*/)
+	if (!is_builtin(p2) && !tmp[nbr_cells].match)
 		tmp[nbr_cells].match = find_predicate(m, p2);
 
 	nbr_cells += p2->nbr_cells;
