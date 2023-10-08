e686fa79d989ceb62a0417b1e849a67aa956be36
Andrew Davison <andrew.davison@gmail.com>
2023-10-09 07:38:58 +1000

    WIP

--- src/predicates.c
+++ src/predicates.c
@@ -3577,6 +3577,7 @@ bool fn_sys_quantum_eraser_2(query *q)
 	tmp2.flags |= FLAG_CSTR_QUANTUM_ERASER;
 	bool ok = unify(q, p1, p1_ctx, &tmp2, q->st.curr_frame);
 	unshare_cell(&tmp2);
+	q->no_tco = true;
 	return ok;
 }
 
