1fa6ebca95e03c882ca25a3630316ed1c10b7615
Andrew Davison <andrew.davison@gmail.com>
2023-11-04 12:53:05 +1000

    WIP

--- samples/chess.pl
+++ samples/chess.pl
@@ -255,7 +255,7 @@ in_check(Board, Color) :-
 %:- entry((move(Board,From,To,Color,Piece) :- nonvar(Board), atom(Color))).
 
 collect_moves(Board, Color, Moves) :-
-    bagof(move(From,To), Piece^move(Board,From,To,Color,Piece), Moves).
+    findall(move(From,To), move(Board,From,To,Color,Piece), Moves).
 
 make_move(Board, From, To, OutBoard) :-
     make_move(Board, From, To, Color, Type, NewBoard),
