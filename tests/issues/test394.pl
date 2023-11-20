05dcb6ef99cdeed3498d07af4e794b39ee0e8773
Andrew Davison <andrew.davison@gmail.com>
2023-11-20 12:56:43 +1000

    Partial fix (still no residuals), add tests

---
+++ tests/issues/test394.pl
@@ -0,0 +1,19 @@
+ti(G=Rs) :-
+   ti(EsG, EsRG, 3),
+   ( G = EsG ; G = EsRG ),
+   findall(R,(call_residue_vars(G,Vs),length(Vs,R)),Rs).
+
+ti(EsG,(A,B,EDif),N) :-
+   N>0,
+   EDif = dif(_,_),
+   EsG = (EDif,A,B),
+   f(EsG).
+
+f((dif(A,B),B=[]*[],A=[]*_)).
+
+run :-
+	findall(G-Rs, ti(G=Rs), L),
+	write(L), nl.
+
+:- initialization(run).
+
