5ea6c204b9b7df106e5b9a11d10929d350280722
Andrew Davison <andrew.davison@gmail.com>
2023-11-29 07:08:38 +1000

    Fix term comparison, re issue #405

--- src/internal.h
+++ src/internal.h
@@ -509,7 +509,7 @@ struct trail_ {
 
 struct slot_ {
 	cell c;
-	uint32_t vgen;
+	uint32_t vgen, vgen2;
 };
 
 // Where 'prev_offset' is the number of frames back
---
+++ tests/issues/test405.pl
@@ -0,0 +1,5 @@
+:-initialization(main).
+
+main :-
+	 A=A*[],B=A*a*[], A\==B,
+	write(ok), nl.
