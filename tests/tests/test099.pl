b9a9e9dfffeecc0aa053e5109ffa763a05a7c87f
Andrew Davison <andrew.davison@gmail.com>
2023-12-21 14:14:06 +1000

    WIP

--- tests/tests/test099.pl
+++ tests/tests/test099.pl
@@ -5,9 +5,7 @@ run :-
 	when(nonvar(A), Run1 = true),
 	when(ground(A), Run2 = true),
 	var(Run1), var(Run2),
-	A = a(B),
-	Run1 == true, var(Run2),
-	B = 1,
-	Run2 == true,
+	A = a(B), Run1 == true,
+	var(Run2),
+	B = 1, Run2 == true,
 	write(ok), nl.
-
