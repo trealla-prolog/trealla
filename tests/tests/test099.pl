65fff550d0a7d56458787ccf47c41ef2616cc258
Andrew Davison <andrew.davison@gmail.com>
2023-12-21 14:11:41 +1000

    WIP

---
+++ tests/tests/test099.pl
@@ -0,0 +1,12 @@
+:- initialization(run).
+:- use_module(library(when)).
+
+run :-
+	when(nonvar(A), Run1 = true),
+	when(ground(A), Run2 = true),
+	var(Run1), var(Run2),
+	A = a(B),
+	Run1 == true, var(Run2),
+	B = 1,
+	Run2 == true.
+
