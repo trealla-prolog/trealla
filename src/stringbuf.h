8f12379bb16fd514da3982dbda323e863f0b44da
Andrew Davison <andrew.davison@gmail.com>
2024-01-30 13:49:16 +1000

    WIP

--- src/stringbuf.h
+++ src/stringbuf.h
@@ -10,31 +10,31 @@
 typedef struct {
 	char tmpbuf[SB_LEN];
 	char *buf, *dst;
-	size_t size;
+	size_t buf_size;
 } stringbuf;
 
 #define SB(pr) stringbuf pr##_buf;								\
-	pr##_buf.size = SB_LEN;										\
+	pr##_buf.buf_size = SB_LEN;									\
 	pr##_buf.buf = pr##_buf.tmpbuf;								\
 	pr##_buf.dst = pr##_buf.buf;								\
 	*pr##_buf.dst = '\0';
 
 #define SB_alloc(pr,len) stringbuf pr##_buf; 					\
-	pr##_buf.size = len;										\
+	pr##_buf.buf_size = len;									\
 	pr##_buf.buf = malloc((len)+1);								\
 	ensure(pr##_buf.buf);										\
 	pr##_buf.dst = pr##_buf.buf;								\
 	*pr##_buf.dst = '\0';
 
 #define SB_check(pr,len) {										\
-	size_t rem = pr##_buf.size - SB_strlen(pr);					\
+	size_t rem = pr##_buf.buf_size - SB_strlen(pr);				\
 	if ((size_t)((len)+1) >= rem) {								\
 		size_t offset = SB_strlen(pr);							\
 		if (pr##_buf.buf != pr##_buf.tmpbuf) {					\
 			pr##_buf.buf = realloc(pr##_buf.buf, 				\
-				(pr##_buf.size += ((len)-rem)) + 256 + 1);		\
+				(pr##_buf.buf_size += ((len)-rem)) + 256 + 1);	\
 		} else {												\
-			pr##_buf.buf = malloc((pr##_buf.size += ((len)-rem)) + 256 + 1); \
+			pr##_buf.buf = malloc((pr##_buf.buf_size += ((len)-rem)) + 256 + 1); \
 			memcpy(pr##_buf.buf, pr##_buf.tmpbuf, offset+1);	\
 		}														\
 		ensure(pr##_buf.buf);									\
@@ -42,11 +42,19 @@ typedef struct {
 	}															\
 }
 
-#define SB_init(pr) 											\
+#define SB_init(pr) {											\
 	pr##_buf.dst = pr##_buf.buf;								\
-	if (pr##_buf.buf) pr##_buf.dst[0] = '\0';
+	if (pr##_buf.buf) pr##_buf.dst[0] = '\0';					\
+}
 
 #define SB_strlen(pr) (pr##_buf.dst - pr##_buf.buf)
+
+#define SB_truncate(pr,len) {									\
+	if ((len) < SB_strlen(pr) {									\
+		pr##_buf.dst = pr##_buf.buf + (len);					\
+		*pr##_buf.dst = '\0';									\
+	}															\
+}
 
 #define SB_trim(pr,ch) {										\
 	if (SB_strlen(pr)) {										\
@@ -103,8 +111,8 @@ typedef struct {
 	}															\
 }
 
-#define SB_fwrite(pr,ptr,size) {								\
-	size_t len = size;											\
+#define SB_fwrite(pr,ptr,buf_size) {							\
+	size_t len = buf_size;										\
 	SB_check(pr, len);											\
 	memcpy(pr##_buf.dst, ptr, len);								\
 	pr##_buf.dst += len;										\
@@ -136,6 +144,6 @@ typedef struct {
 #define SB_free(pr) {											\
 	if (pr##_buf.buf != pr##_buf.tmpbuf)						\
 		free(pr##_buf.buf);										\
-	pr##_buf.size = 0;											\
+	pr##_buf.buf_size = 0;										\
 	pr##_buf.buf = pr##_buf.dst = NULL;							\
 }
