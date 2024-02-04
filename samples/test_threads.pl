5dbd20bdb0d1e8c93adca49aa9f23f8c1738b42a
Andrew Davison <andrew.davison@gmail.com>
2024-02-04 14:48:14 +1000

    WIP

--- samples/test_threads.pl
+++ samples/test_threads.pl
@@ -4,20 +4,20 @@ main :-
 	mutex_create(_, [alias(bar)]),
 	between(1, 10, I),
 		atomic_concat(foo, I, Alias),
-		format("Starting ~w~n", [Alias]),
+		format("Starting ~w~n", [I]),
 		thread_create(thread_run(I), _, [alias(Alias)]),
 		fail.
 main :-
 	between(1, 10, I),
 		atomic_concat(foo, I, Alias),
 		Msg = msg(Alias),
-		format(" ...Sending ~w ~w~n", [Alias, Msg]),
+		format(" ...Sending ~w ~w~n", [I, Msg]),
 		thread_send_message(Alias, Msg),
 		fail.
 main :-
 	between(1, 10, I),
 		atomic_concat(foo, I, Alias),
-		format("Joining ~w~n", [Alias]),
+		format("Joining ~w~n", [I]),
 		thread_join(Alias,_),
 		fail.
 main :-
