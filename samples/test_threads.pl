4dd38ff280b05259266bae37047b09ecb1a729fc
Andrew Davison <andrew.davison@gmail.com>
2024-02-04 13:44:03 +1000

    WIP

--- samples/test_threads.pl
+++ samples/test_threads.pl
@@ -4,26 +4,20 @@ main :-
 	mutex_create(_, [alias(bar)]),
 	between(1, 10, I),
 		atomic_concat(foo, I, Alias),
-		mutex_lock(bar),
 		format("Starting ~w~n", [Alias]),
-		mutex_unlock(bar),
 		thread_create(thread_run(I), _, [alias(Alias)]),
 		fail.
 main :-
 	between(1, 10, I),
 		atomic_concat(foo, I, Alias),
 		Msg = ok,
-		mutex_lock(bar),
 		format(" ...Sending ~w msg ~w~n", [Alias, Msg]),
-		mutex_unlock(bar),
 		thread_send_message(Alias, Msg),
 		fail.
 main :-
 	between(1, 10, I),
 		atomic_concat(foo, I, Alias),
-		mutex_lock(bar),
 		format("Joining ~w~n", [Alias]),
-		mutex_unlock(bar),
 		thread_join(Alias,_),
 		fail.
 main :-
@@ -31,12 +25,10 @@ main :-
 	writeln(done).
 
 thread_run(I) :-
-	mutex_lock(bar),
 	format(" ...Thread ~w~n", [I]),
+	mutex_lock(bar),
 	mutex_unlock(bar),
 	thread_get_message(Msg),
-	mutex_lock(bar),
 	format(" ...Got ~w got ~w~n", [I, Msg]),
-	mutex_unlock(bar),
 	true.
 
