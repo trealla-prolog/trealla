426565ba0c9daa6ffa7f67b0470e9f4e299e60c4
Andrew Davison <andrew.davison@gmail.com>
2024-02-09 09:47:52 +1000

    WIP

--- src/bif_threads.c
+++ src/bif_threads.c
@@ -741,10 +741,13 @@ static bool bif_thread_create_3(query *q)
 	cell *goal = deep_clone_to_heap(q, p1, p1_ctx);
 	check_heap_error(goal);
 	t->nbr_vars = rebase_vars(q, goal, 0);
-	cell *tmp2 = alloc_on_heap(q, 1 + goal->nbr_cells + 2);
+	cell *tmp2 = alloc_on_heap(q, 2+goal->nbr_cells+2);
 	check_heap_error(tmp2);
 	pl_idx nbr_cells = 0;
-	make_struct(tmp2+nbr_cells++, g_conjunction_s, bif_iso_conjunction_2, 2, goal->nbr_cells+2);
+	make_struct(tmp2+nbr_cells, g_conjunction_s, bif_iso_conjunction_2, 2, 1+goal->nbr_cells+2);
+	SET_OP(tmp2+nbr_cells, OP_XFY);
+	nbr_cells++;
+	make_struct(tmp2+nbr_cells++, new_atom(q->pl, "ignore"), bif_ignore_1, 1, goal->nbr_cells);
 	nbr_cells += copy_cells(tmp2+nbr_cells, goal, goal->nbr_cells);
 	make_struct(tmp2+nbr_cells++, new_atom(q->pl, "halt"), bif_iso_halt_0, 0, 0);
 	make_call(q, tmp2+nbr_cells);
@@ -766,14 +769,17 @@ static bool bif_thread_create_3(query *q)
 		cell *goal = deep_clone_to_heap(q, p4, p4_ctx);
 		check_heap_error(goal);
 		t->at_exit_nbr_vars = rebase_vars(q, goal, 0);
-		cell *tmp2 = alloc_on_heap(q, 1 + goal->nbr_cells + 2);
+		cell *tmp2 = alloc_on_heap(q, 2+goal->nbr_cells+2);
 		check_heap_error(tmp2);
 		pl_idx nbr_cells = 0;
-		make_struct(tmp2+nbr_cells++, g_conjunction_s, bif_iso_conjunction_2, 2, goal->nbr_cells+2);
+		make_struct(tmp2+nbr_cells, g_conjunction_s, bif_iso_conjunction_2, 2, 1+goal->nbr_cells+2);
+		SET_OP(tmp2+nbr_cells, OP_XFY);
+		nbr_cells++;
+		make_struct(tmp2+nbr_cells++, new_atom(q->pl, "ignore"), bif_ignore_1, 1, goal->nbr_cells);
 		nbr_cells += copy_cells(tmp2+nbr_cells, goal, goal->nbr_cells);
 		make_struct(tmp2+nbr_cells++, new_atom(q->pl, "halt"), bif_iso_halt_0, 0, 0);
 		make_call(q, tmp2+nbr_cells);
-		//DUMP_TERM("at_exit", at_exit, q->st.curr_frame, 0);
+		//DUMP_TERM("at_exit", tmp2, q->st.curr_frame, 0);
 		t->at_exit = deep_clone_to_heap(t->q, tmp2, 0);
 	}
 
