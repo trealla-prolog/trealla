2e958ad7ce213ab0a23d6e0fce4a37f07acef17b
Andrew Davison <andrew.davison@gmail.com>
2024-02-09 13:17:51 +1000

    WIP

--- src/bif_threads.c
+++ src/bif_threads.c
@@ -63,7 +63,7 @@ struct pl_thread_ {
 static pl_thread g_pl_threads[MAX_THREADS] = {0};
 static unsigned g_pl_any_threads = 0;
 
-#define THREAD_DEBUG if (1)
+#define THREAD_DEBUG if (0)
 
 #define is_thread(c) is_thread_or_alias(q, c)
 #define is_mutex(c) is_mutex_or_alias(q, c)
