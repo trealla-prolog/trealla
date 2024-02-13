afe1d2786ecf444bdd16c142054e1526dbb74f19
Andrew Davison <andrew.davison@gmail.com>
2024-02-13 16:44:00 +1000

    WIP

--- samples/test_threads.pl
+++ samples/test_threads.pl
@@ -1,6 +1,6 @@
 :- initialization(main).
 
-threads(8).
+threads(4).
 
 main :-
 	threads(THREADS),
@@ -11,11 +11,35 @@ main :-
 		fail.
 main :-
 	threads(THREADS),
+	thread_self(Self),
 	between(1, THREADS, I),
 		atomic_concat(foo, I, Alias),
-		Msg = msg(Alias),
-		format(" ...Sending ~w ~w~n", [I, Msg]),
+		Msg = msg(Alias, from(Self)),
+		format(" ...Sending1 ~w ~w~n", [I, Msg]),
 		thread_send_message(Alias, Msg),
+		fail.
+main :-
+	threads(THREADS),
+	between(1, THREADS, I),
+		format("Getting1 ~w~n", [I]),
+		thread_get_message(Msg),
+		format("... got ~w ~w~n", [I,Msg]),
+		fail.
+main :-
+	threads(THREADS),
+	thread_self(Self),
+	between(1, THREADS, I),
+		atomic_concat(foo, I, Alias),
+		Msg = msg(Alias, from(Self)),
+		format(" ...Sending2 ~w ~w~n", [I, Msg]),
+		thread_send_message(Alias, Msg),
+		fail.
+main :-
+	threads(THREADS),
+	between(1, THREADS, I),
+		format("Getting2 ~w~n", [I]),
+		thread_get_message(Msg),
+		format("... got ~w ~w~n", [I,Msg]),
 		fail.
 main :-
 	threads(THREADS),
@@ -28,9 +52,14 @@ main :-
 	writeln(done).
 
 thread_run(I) :-
-	format(" ...Thread ~w~n", [I]),
-	thread_get_message(Msg),
-	(between(1,1000000,_), fail ; true),
-	format(" ...Got ~w got ~w~n", [I, Msg]),
-	true.
+	between(1,2,_),
+		format(" ...Thread ~w~n", [I]),
+		thread_get_message(Msg),
+		format(" ...Got ~w got ~w~n", [I, Msg]),
+		(between(1,1000000,_), fail; true),
+		Msg = msg(_, from(Chan)),
+		thread_send_message(Chan, ok),
+		fail.
+thread_run(I) :-
+	format("Terminating ~w~n", [I]).
 
