954df8a82f9fc63f99b360556484df006727f30c
Andrew Davison <andrew.davison@gmail.com>
2024-02-13 16:18:22 +1000

    WIP

--- samples/test_threads.pl
+++ samples/test_threads.pl
@@ -3,8 +3,6 @@
 threads(8).
 
 main :-
-	mutex_create(_, [alias(bar)]),
-	mutex_property(bar, status(unlocked)),
 	threads(THREADS),
 	between(1, THREADS, I),
 		atomic_concat(foo, I, Alias),
@@ -27,14 +25,12 @@ main :-
 		thread_join(Alias,_),
 		fail.
 main :-
-	mutex_destroy(bar),
 	writeln(done).
 
 thread_run(I) :-
 	format(" ...Thread ~w~n", [I]),
-	mutex_lock(bar),
-	mutex_unlock(bar),
 	thread_get_message(Msg),
+	(between(1,1000000,_), fail ; true),
 	format(" ...Got ~w got ~w~n", [I, Msg]),
 	true.
 
