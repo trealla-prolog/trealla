d2e2532d9bb07a206e89ab3901fc0fc6e6a2af22
Andrew Davison <andrew.davison@gmail.com>
2024-02-19 13:29:24 +1000

    Some fixes for threads

--- src/prolog.c
+++ src/prolog.c
@@ -553,9 +553,8 @@ void pl_destroy(prolog *pl)
 	if (!--g_tpl_count)
 		g_destroy();
 
-	if (pl->q_cnt) {
+	if (pl->q_cnt && isatty(fileno(stdout))) {
 		printf("*** Oops, outstanding queries: %u\n", (unsigned)pl->q_cnt);
-		sleep(1);
 	}
 
 	if (!pl->q_cnt)
