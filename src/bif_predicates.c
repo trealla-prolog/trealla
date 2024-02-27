25e81833a54943377a40bf285f833a7ed51ac402
Andrew Davison <andrew.davison@gmail.com>
2024-02-27 17:14:40 +1000

    REVERT (partial): Reworking copy_term/2 & friends + some fixes

--- src/bif_predicates.c
+++ src/bif_predicates.c
@@ -1999,6 +1999,9 @@ static bool do_duplicate_term(query *q, bool copy_attrs)
 	cell *tmp2 = deep_copy_to_heap(q, p1, p1_ctx, copy_attrs);
 	check_heap_error(tmp2);
 
+#if 1
+	return unify(q, p2, p2_ctx, tmp2, q->st.curr_frame);
+#else
 	if (!q->cycle_error)
 		return unify(q, p2, p2_ctx, tmp2, q->st.curr_frame);
 
@@ -2009,6 +2012,7 @@ static bool do_duplicate_term(query *q, bool copy_attrs)
 		return false;
 
 	return unify(q, tmp, from_ctx, tmp2, q->st.curr_frame);
+#endif
 }
 
 // Do copy attributes
