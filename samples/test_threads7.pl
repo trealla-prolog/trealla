ac5878672d1c0a7acaefc8048c7d196edc536773
Andrew Davison <andrew.davison@gmail.com>
2024-03-18 21:54:35 +1000

    WIP

---
+++ samples/test_threads7.pl
@@ -0,0 +1,33 @@
+:- initialization(main).
+
+loops(1000).
+
+main :-
+	message_queue_create(_, [alias(foo)]),
+	format("Starting~n", []),
+	thread_create(thread_run(1), _, [alias(bar)]),
+	thread_create(thread_run(2), _, [alias(baz)]),
+	delay(1),
+	fail.
+main :-
+	loops(LOOPS),
+	between(1, LOOPS, I),
+		%format(" ...Sending ~w~n", [I]),
+		thread_send_message(foo, foo(1)),
+		thread_send_message(foo, foo(2)),
+		fail.
+main :-
+	thread_join(bar, _),
+	thread_join(baz, _),
+	true.
+
+thread_run(J) :-
+	format(" ...Thread ~w~n", [J]),
+	loops(LOOPS),
+	between(1,LOOPS,I),
+		thread_get_message(foo, foo(J)),
+		%format(" ...Got ~w ~w~n", [I, foo(J)]),
+		fail.
+thread_run(J) :-
+	format("Terminating ~w~n", [J]),
+	true.
