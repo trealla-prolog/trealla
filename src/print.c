f9cd50587ca781b9b9bd20afd0e4be47f83e6168
Andrew Davison <andrew.davison@gmail.com>
2024-04-14 17:45:17 +1000

    WIP

--- src/print.c
+++ src/print.c
@@ -466,7 +466,7 @@ static void print_variable(query *q, cell *c, pl_idx c_ctx, bool running)
 	} else if (q->portray_vars || (q->is_dump_vars && q->cycle_error)) {
 		SB_sprintf(q->sb, "%s", get_slot_name(q, slot_nbr));
 	} else if (q->is_dump_vars) {
-		if ((c_ctx == 0) && !is_fresh(c) && !is_anon(c) && (c->var_nbr < q->p->nbr_vars)) {
+		if ((c_ctx == 0) && (c->var_nbr < q->p->nbr_vars)) {
 			SB_sprintf(q->sb, "%s", q->p->vartab.var_name[c->var_nbr]);
 		} else {
 			SB_sprintf(q->sb, "_%s", get_slot_name(q, slot_nbr));
