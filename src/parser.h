bbf19221bdbcdac1be3e8ec9fa0eb11126d94680
Andrew Davison <andrew.davison@gmail.com>
2024-06-07 13:47:57 +1000

    WIP

--- src/parser.h
+++ src/parser.h
@@ -21,18 +21,17 @@ void make_uint(cell *tmp, pl_uint v);
 void make_int(cell *tmp, pl_int v);
 void make_float(cell *tmp, pl_flt v);
 void make_ptr(cell *tmp, void *v);
-void make_struct_(cell *tmp, pl_idx offset, unsigned arity, pl_idx extra_cells);
 void make_var(cell *tmp, pl_idx off, unsigned var_nbr);
 void make_ref(cell *tmp, unsigned var_nbr, pl_idx ctx);
 void make_end(cell *tmp);
 void make_atom(cell *tmp, pl_idx offset);
-cell *make_nil(void);
 void make_smalln(cell *tmp, const char *s, size_t n);
 bool make_cstringn(cell *tmp, const char *s, size_t n);
 bool make_stringn(cell *tmp, const char *s, size_t n);
 void make_blob(cell *tmp, void *ptr);
 void make_dbref(cell *tmp, void *ptr);
 void make_kvref(cell *tmp, void *ptr);
+cell *make_nil(void);
 
 void clear_clause(clause *t);
 void do_reduce(cell *n);
@@ -44,6 +43,8 @@ bool do_register_predicate(module *m, query *q, void *handle, const char *symbol
 bool do_register_struct(module *m, query *q, void *handle, const char *symbol, cell *l, pl_idx l_ctx, const char *ret);
 int do_dlclose(void *handle);
 #endif
+
+void make_struct_(cell *tmp, pl_idx offset, unsigned arity, pl_idx extra_cells);
 
 #define make_struct(tmp, offset, fn, arity, extra_cells) { \
 	cell *tmp_make = tmp; \
