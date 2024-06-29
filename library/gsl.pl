3ef5862e333a127cbd67a5e06c7df52c4bb57b11
Andrew Davison <andrew.davison@gmail.com>
2024-06-29 10:17:10 +1000

    Add GNU Scientific Library (under development)

--- library/gsl.pl
+++ library/gsl.pl
@@ -1,13 +1,23 @@
 :- module(gsl, [
 	gsl_matrix_calloc/3,
-	gsl_matrix_free/1
+	gsl_matrix_free/1,
+	gsl_matrix_memcpy/3,
+	gsl_matrix_set/4,
+	gsl_matrix_get/4,
+	gsl_matrix_set_all/2,
+	gsl_matrix_set_zero/1,
+	gsl_matrix_set_identity/1,
+	gsl_linalg_LU_solve/5,
+	gsl_linalg_LU_det/3
 	]).
 
 % GNU Scientific Library
 %
-% USAGE UNDER DEVELOPENT, EXPERIMENTAL
+% UNDER DEVELOPENT, EXPERIMENTAL
 %
 % UBUNTU: sudo apt install libgsl-dev
+%
+% REF: https://www.gnu.org/software/gsl/doc/html/index.html
 
 /*
 	$ tpl
@@ -19,5 +29,15 @@
 
 :- use_foreign_module('libgsl.so', [
 	gsl_matrix_calloc([uint64,uint64], ptr),
-	gsl_matrix_free([ptr], void)
+	gsl_matrix_free([ptr], void),
+	gsl_matrix_memcpy([ptr,ptr], sint),
+
+	gsl_matrix_set([ptr,uint64,uint64,fp64], void),
+	gsl_matrix_get([ptr,uint64,uint64], fp64),
+	gsl_matrix_set_all([ptr,fp64], void),
+	gsl_matrix_set_zero([ptr], void),
+	gsl_matrix_set_identity([ptr], void),
+
+	gsl_linalg_LU_solve([ptr,ptr,ptr,ptr], sint),
+	gsl_linalg_LU_det([ptr,sint], fp64)
 	]).
