e5016558020c837c8559bfdce00eebce45df4db6
Andrew Davison <andrew.davison@gmail.com>
2024-07-02 14:38:12 +1000

    Use linear storage for dense matrices, for speed

--- library/gsl.pl
+++ library/gsl.pl
@@ -119,7 +119,7 @@
 vec_write(V,S) :- '$gsl_vector_write'(V,S).
 
 vec_read(V,S,Size1) :-
-	'$gsl_matrix_alloc'(S,Size1),
+	'$gsl_vector_alloc'(S,Size1),
 	gsl_vector_alloc(Size1,V),
 	'$gsl_vector_read'(V,S).
 
