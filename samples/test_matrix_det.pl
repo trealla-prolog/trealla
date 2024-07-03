256df73a418dfba1e0dde81130b2a9ea906348c1
Andrew Davison <andrew.davison@gmail.com>
2024-07-03 13:23:20 +1000

    WIP

--- samples/test_matrix_det.pl
+++ samples/test_matrix_det.pl
@@ -23,6 +23,7 @@ mat_lup_det(M,Size,Det) :-
 main :-
 	open('samples/test_matrix_det.mat',read,S),
 	mat_read(M,S,Rows,Cols),
+	Rows =:= Cols,
 	mat_lup_det(M,Rows,Det),
 	gsl_matrix_free(M),
 	close(S),
