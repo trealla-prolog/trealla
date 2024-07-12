b5ea57cfa9ddae7d09908e5ef55e18c9b5713316
Andrew Davison <andrew.davison@gmail.com>
2024-07-12 23:04:22 +1000

    WIP

--- src/bif_ffi.c
+++ src/bif_ffi.c
@@ -72,9 +72,11 @@ typedef struct foreign_struct_ {
 
 static foreign_struct g_ffi_structs[MAX_FFI] = {{0}};
 
+#if USE_FFI
 typedef struct nested_elements {
 	ffi_type *elements[MAX_FFI_ARGS];
 } nested_elements;
+#endif
 
 #if USE_FFI
 void *do_dlopen(const char *filename, int flag)
