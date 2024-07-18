4da4ec334d0eb1f7b1f57ef6d95aa66933f028b9
Andrew Davison <andrew.davison@gmail.com>
2024-07-18 21:22:57 +1000

    On a write skip out on a yield retry (because it was done already), re issue #569

--- src/bif_streams.c
+++ src/bif_streams.c
@@ -1902,6 +1902,9 @@ static bool bif_iso_flush_output_1(query *q)
 
 static bool bif_iso_nl_0(query *q)
 {
+	if (q->retry)
+		return true;
+
 	int n = q->pl->current_output;
 	stream *str = &q->pl->streams[n];
 	fputc('\n', str->fp);
@@ -1915,6 +1918,9 @@ static bool bif_iso_nl_0(query *q)
 
 static bool bif_iso_nl_1(query *q)
 {
+	if (q->retry)
+		return true;
+
 	GET_FIRST_ARG(pstr,stream);
 	int n = get_stream(q, pstr);
 	stream *str = &q->pl->streams[n];
