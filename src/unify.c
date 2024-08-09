259aff8248d2f771e58c403af293bad315374ccd
Andrew Davison <andrew.davison@gmail.com>
2024-08-09 20:10:28 +1000

    WIP

--- src/unify.c
+++ src/unify.c
@@ -332,8 +332,7 @@ static void set_var(query *q, const cell *c, pl_idx c_ctx, cell *v, pl_idx v_ctx
 		make_ref(&e->c, v->var_nbr, v_ctx);
 
 		if (c_ctx == q->st.fp) {
-			if (!is_local(c))
-				q->no_tco = true;
+			q->no_tco = true;
 		}
 	} else if (is_compound(v)) {
 		make_indirect(&e->c, v, v_ctx);
