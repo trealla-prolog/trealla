1b2a0605039c601d44d73ec8f4dc3a1648bd0c3b
Andrew Davison <andrew.davison@gmail.com>
2024-09-18 14:31:16 +1000

    WIP

--- src/bif_control.c
+++ src/bif_control.c
@@ -589,11 +589,7 @@ static bool bif_shift_1(query *q)
 	dup_cells_by_ref(tmp2+1, next, q->st.curr_frame, next->nbr_cells);
 	q->cont = tmp2;
 	q->cont_ctx = q->st.curr_frame;
-
-	if (!find_reset_handler(q))
-		return false;
-
-	return true;
+	return find_reset_handler(q);
 }
 
 bool bif_sys_call_cleanup_3(query *q)
