2a0555baf430922ee6ecc85e4d57fe43ff624dc0
Andrew Davison <andrew.davison@gmail.com>
2024-09-19 10:41:31 +1000

    Fix op/3, re issue #596

--- src/bif_predicates.c
+++ src/bif_predicates.c
@@ -2766,7 +2766,7 @@ static bool do_op(query *q, cell *p3, pl_idx p3_ctx)
 	unsigned tmp_pri = match_op(q->st.m, C_STR(q, p3), &tmp_optype, p3->arity);
 
 	if (IS_INFIX(specifier) && IS_POSTFIX(tmp_optype))
-		return throw_error(q, p3, p3_ctx, "permission_error", "create,operator1");
+		return throw_error(q, p3, p3_ctx, "permission_error", "create,operator");
 
 	if (!tmp_pri && !pri)
 		return true;
