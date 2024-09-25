47c6e90b8fbff37a24af8884ed238b7104bf054e
Andrew Davison <andrew.davison@gmail.com>
2024-09-26 08:05:40 +1000

    WIP

---
+++ samples/tco.pl
@@ -0,0 +1,18 @@
+test1(0) :- !, statistics, nl, fail.
+test1(N) :- N1 is N-1, test1(N1).
+
+f.
+
+test2(0) :- !, statistics, nl, fail.
+test2(N) :- f, N1 is N-1, test2(N1).
+
+% No local vars & no structures, yet not recovered...
+
+f(_, _).
+
+test3(0) :- !, statistics, fail.
+test3(I) :- f(I, _), I2 is I-1, test3(I2).
+
+main :-
+	test1(100000); test2(200000); test3(300000); true.
+
