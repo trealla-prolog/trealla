0953a74ecd4e360aca294b0353ac99d791b7ac57
Andrew Davison <andrew.davison@gmail.com>
2024-09-29 18:11:01 +1000

    WIP

--- src/query.c
+++ src/query.c
@@ -708,7 +708,9 @@ static void commit_frame(query *q)
 
 	// Matching a fact (see disjunction in bif_control.c)...
 
-	if (q->pl->opt && last_match && !body && !cl->nbr_vars && !q->st.pr->key.arity) {
+	if (q->pl->opt && last_match && !body && !cl->nbr_vars
+		&& !q->st.pr->key.arity
+		) {
 		leave_predicate(q, q->st.pr);
 		drop_choice(q);
 		//cut(q); 				// ???
