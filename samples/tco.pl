ffa547adad4c13d1cc0b27d5437831664298f4ef
Andrew Davison <andrew.davison@gmail.com>
2024-10-01 11:37:24 +1000

    WIP

--- samples/tco.pl
+++ samples/tco.pl
@@ -1,20 +1,22 @@
-test1(0) :- !, statistics, nl, fail.
+test1(0) :- !, statistics, fail.
 test1(N) :- N1 is N-1, test1(N1).
 
 f.
 
-test2(0) :- !, statistics, nl, fail.
+test2(0) :- !, statistics, fail.
 test2(N) :- f, N1 is N-1, test2(N1).
 
 f(1).
 
-test3(0) :- !, statistics, nl, fail.
+test3(0) :- !, statistics, fail.
 test3(N) :- f(_), N1 is N-1, test3(N1).
 
 f(_, _).
 
 test4(0) :- !, statistics, fail.
 test4(I) :- f(I, _), I2 is I-1, test4(I2).
+
+statistics.
 
 main :-
 	test1(100000);
