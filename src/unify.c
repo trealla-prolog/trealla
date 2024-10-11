2e93e5c480196d5a83bb7ff9e6301d15c32a6007
Andrew Davison <andrew.davison@gmail.com>
2024-10-11 16:00:30 +1000

    WIP

--- src/unify.c
+++ src/unify.c
@@ -325,9 +325,6 @@ static void set_var(query *q, const cell *c, pl_idx c_ctx, cell *v, pl_idx v_ctx
 	if (is_compound(v) && (v_ctx == q->st.fp))
 		q->no_fact = true;
 
-	if (is_managed(v))
-		q->no_fact = true;
-
 	// If anything outside the current frame points inside the
 	// current frame then we can't TCO.
 	// If anything points inside the next frame then ditto.
