cae7d440894ce398c50fea4c03c188487ae36384
Andrew Davison <andrew.davison@gmail.com>
2024-12-13 09:16:31 +1000

    WIP

--- src/parser.c
+++ src/parser.c
@@ -339,34 +339,6 @@ static void compile_term(clause *cl, cell **dst, cell **src)
 		compile_term(cl, dst, src);		// LHS
 		compile_term(cl, dst, src);		// RHS
 		return;
-#if 0
-	} else if ((*src)->val_off == g_negation_s) {
-		unsigned var_nbr = cl->nbr_vars++;
-		*src += 1;
-		cell *save_dst = *dst;
-		make_instr((*dst)++, g_sys_succeed_on_retry_s, bif_sys_succeed_on_retry_2, 2, 2);
-		make_var((*dst)++, g_anon_s, var_nbr);
-		make_uint((*dst)++, 0);										// Dummy value
-		compile_term(cl, dst, src);
-		make_instr((*dst)++, g_cut_s, bif_iso_cut_0, 0, 0);
-		make_instr((*dst)++, g_sys_drop_barrier_s, bif_sys_drop_barrier_1, 1, 1);
-		make_var((*dst)++, g_anon_s, var_nbr);
-		make_instr((*dst)++, g_fail_s, bif_iso_fail_0, 0, 0);
-		make_uint(save_dst+2, *dst - save_dst);						// Real value
-		return;
-#endif
-#if 0
-	} else if (((*src)->val_off == g_call_s) && ((*src)->arity == 1)) {
-		unsigned var_nbr = cl->nbr_vars++;
-		*src += 1;
-		cell *save_dst = *dst;
-		make_instr((*dst)++, g_sys_succeed_on_retry_s, bif_sys_fail_on_retry_1, 1, 1);
-		make_var((*dst)++, g_anon_s, var_nbr);
-		compile_term(cl, dst, src);
-		make_instr((*dst)++, g_sys_drop_barrier_s, bif_sys_drop_barrier_1, 1, 1);
-		make_var((*dst)++, g_anon_s, var_nbr);
-		return;
-#endif
 	} else {
 		pl_idx n = copy_cells(*dst, *src, (*src)->nbr_cells);
 		*dst += n;
