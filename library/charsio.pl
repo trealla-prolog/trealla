27cc2931a119d89ae23c53c320af4284acfa3755
Andrew Davison <andrew.davison@gmail.com>
2025-03-16 09:24:29 +1000

    Fix charsio:get_n_chars/3, re issue #679

--- library/charsio.pl
+++ library/charsio.pl
@@ -15,7 +15,7 @@ get_line_to_chars(Stream, Cs0, Cs) :-
 
 get_n_chars(Stream, N, Cs) :-
     bread(Stream, N, Cs0),
-    Cs0 == Cs.
+    Cs0 = Cs.
 
 read_from_chars(Cs, T) :-
     read_term_from_chars(Cs, T, []).
