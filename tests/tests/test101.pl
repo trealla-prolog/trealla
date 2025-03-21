5faaf3b7540de1e125970c19bff0c3a695f5e51b
Andrew Davison <andrew.davison@gmail.com>
2025-03-21 13:46:09 +1000

    WIP

--- tests/tests/test101.pl
+++ tests/tests/test101.pl
@@ -1,24 +1,24 @@
 :- initialization(main).
 
-test1(0) :- !, statistics(frames, Fs), statistics(choices, Cs), statistics(trails, Ts), statistics(slots, Ss), write([Fs,Cs,Ts,Ss]), nl, fail.
+test1(0) :- !, statistics(frames, Fs), statistics(choices, Cs), statistics(trails, Ts), statistics(slots, Ss), write([f,Fs,c,Cs,t,Ts,s,Ss]), nl, fail.
 test1(N) :- N1 is N-1, test1(N1).
 
 f :- true.
-test2(0) :- !, statistics(frames, Fs), statistics(choices, Cs), statistics(trails, Ts), statistics(slots, Ss), write([Fs,Cs,Ts,Ss]), nl, fail.
+test2(0) :- !, statistics(frames, Fs), statistics(choices, Cs), statistics(trails, Ts), statistics(slots, Ss), write([f,Fs,c,Cs,t,Ts,s,Ss]), nl, fail.
 test2(N) :- f, N1 is N-1, test2(N1).
 
-f(1) :- true.
-test3(0) :- !, statistics(frames, Fs), statistics(choices, Cs), statistics(trails, Ts), statistics(slots, Ss), write([Fs,Cs,Ts,Ss]), nl, fail.
-test3(N) :- f(_), N1 is N-1, test3(N1).
+f(_) :- true.
+test3(0) :- !, statistics(frames, Fs), statistics(choices, Cs), statistics(trails, Ts), statistics(slots, Ss), write([f,Fs,c,Cs,t,Ts,s,Ss]), nl, fail.
+test3(N) :- f(N), N1 is N-1, test3(N1).
 
 % TODO
 
 f(_, _) :- true.
-test4(0) :- !, statistics(frames, Fs), statistics(choices, Cs), statistics(trails, Ts), statistics(slots, Ss), write([Fs,Cs,Ts,Ss]), nl, fail.
+test4(0) :- !, statistics(frames, Fs), statistics(choices, Cs), statistics(trails, Ts), statistics(slots, Ss), write([f,Fs,c,Cs,t,Ts,s,Ss]), nl, fail.
 test4(N) :- f(N, _), N1 is N-1, test4(N1).
 
 f(_, g(_), g(g(_))) :- true.
-test5(0) :- !, statistics(frames, Fs), statistics(choices, Cs), statistics(trails, Ts), statistics(slots, Ss), write([Fs,Cs,Ts,Ss]), nl, fail.
+test5(0) :- !, statistics(frames, Fs), statistics(choices, Cs), statistics(trails, Ts), statistics(slots, Ss), write([f,Fs,c,Cs,t,Ts,s,Ss]), nl, fail.
 test5(N) :- f(I, g(N), g(g(_))), N1 is N-1, test5(N1).
 
 statistics.
