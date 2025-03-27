663aa166cc3f7b7b1def7e1f635eb7ad59035337
Andrew Davison <andrew.davison@gmail.com>
2025-03-27 12:50:40 +1000

    WIP

--- library/builtins.pl
+++ library/builtins.pl
@@ -361,13 +361,13 @@ recorded(K, V) :- nonvar(K), '$record_global_key'(K,V).
 recorded(K, V, R) :- nonvar(K), clause('$record_global_key'(K,V), _, R).
 current_key(K) :- var(K), '$record_global_key'(K,_).
 
-:- help(recorda(+term,+term), [iso(false)]).
-:- help(recorda(+term,+term,-ref), [iso(false)]).
-:- help(recordz(+term,+term), [iso(false)]).
-:- help(recordz(+term,+term,-ref), [iso(false)]).
-:- help(recorded(+term,?term), [iso(false)]).
-:- help(recorded(+term,?term,-ref), [iso(false)]).
-:- help(current_key(-term), [iso(false)]).
+:- help(recorda(+term,+term), [iso(false),deprecated(true)]).
+:- help(recorda(+term,+term,-ref), [iso(false),deprecated(true)]).
+:- help(recordz(+term,+term), [iso(false),deprecated(true)]).
+:- help(recordz(+term,+term,-ref), [iso(false),deprecated(true)]).
+:- help(recorded(+term,?term), [iso(false),deprecated(true)]).
+:- help(recorded(+term,?term,-ref), [iso(false),deprecated(true)]).
+:- help(current_key(-term), [iso(false),deprecated(true)]).
 
 :- meta_predicate(call_with_time_limit(+,0)).
 
