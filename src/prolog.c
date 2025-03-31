5474e0257bde383ac75b336a0ee880983fbac1be
Andrew Davison <andrew.davison@gmail.com>
2025-03-31 13:52:32 +1000

    Cyclic terms top-level (try this for stack)

--- src/prolog.c
+++ src/prolog.c
@@ -603,7 +603,7 @@ static bool g_init(prolog *pl)
 #if !defined(_WIN32) && !defined(__wasi__) && !defined(__ANDROID__)
 	struct rlimit rlp;
 	getrlimit(RLIMIT_STACK, &rlp);
-	g_max_depth = rlp.rlim_cur / 1024 / 2; // divide by 2 for safety
+	g_max_depth = (rlp.rlim_cur - 1024) / 1024;
 #endif
 
 	return error;
