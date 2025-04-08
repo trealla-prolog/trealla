f9b0737f34b801614b2c96ff3db26a1324bdc200
Andrew Davison <andrew.davison@gmail.com>
2025-04-08 21:10:54 +1000

    WIP

--- library/builtins.pl
+++ library/builtins.pl
@@ -531,10 +531,8 @@ read_term_from_chars_(T, Opts, Cs, Rest) :-
 	'$read_term_from_chars'(T, Opts, Cs, Rest).
 
 read_from_atom(A, T) :- read_term_from_atom(A, T, []).
-read_from_chars(Cs, T) :- read_term_from_chars(Cs, T, []).
 
 :- help(read_from_atom(+atom,?term), [iso(false)]).
-:- help(read_from_chars(+chars,?term), [iso(false)]).
 
 with_output_to(chars(Cs), Goal) :-
 	setup_call_cleanup(
