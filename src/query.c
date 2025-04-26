8ca65ee72b02afcc1f3035ad71db52035d6f09ca
Andrew Davison <andrew.davison@gmail.com>
2025-04-26 23:04:10 +1000

    WIP

--- src/query.c
+++ src/query.c
@@ -810,7 +810,7 @@ bool push_barrier(query *q)
 bool push_succeed_on_retry_with_barrier(query *q, pl_idx skip)
 {
 	frame *f = GET_CURR_FRAME();
-	f->unify_no_tco = true;				// FIXME
+	f->unify_no_tco = true;				// FIXME: memory waste
 	check_heap_error(push_barrier(q));
 	choice *ch = GET_CURR_CHOICE();
 	ch->succeed_on_retry = true;
