183ed13b9031a76a11608d4d37c77c2aa9b89563
Andrew Davison <andrew.davison@gmail.com>
2025-05-19 08:43:14 +1000

    WIP

--- library/lists.pl
+++ library/lists.pl
@@ -589,10 +589,18 @@ tasklist_([E1|T1], [E2|T2], [E3|T3], [E4|T4], [E5|T5], [E6|T6], G) :-
 tasklist(G, L1, L2, L3, L4, L5, L6, L7) :-
 	tasklist_(L1, L2, L3, L4, L5, L6, L7, G).
 
+tasklist(G, L1, L2, L3, L4, L5, L6, L7, L8) :-
+	tasklist_(L1, L2, L3, L4, L5, L6, L7, L8, G).
+
 tasklist_([], [], [], [], [], [], [], _) :- wait.
 tasklist_([E1|T1], [E2|T2], [E3|T3], [E4|T4], [E5|T5], [E6|T6], [E7|T7], G) :-
 	call_task(G, E1, E2, E3, E4, E5, E6, E7),
 	tasklist_(T1, T2, T3, T4, T5, T6, T7, G).
+
+tasklist_([], [], [], [], [], [], [], [], _) :- wait.
+tasklist_([E1|T1], [E2|T2], [E3|T3], [E4|T4], [E5|T5], [E6|T6], [E7|T7], [E8|T8], G) :-
+	call_task(G, E1, E2, E3, E4, E5, E6, E7, E8),
+	tasklist_(T1, T2, T3, T4, T5, T6, T7, T8, G).
 
 /*
 :- meta_predicate(tasklist(1, ?)).
@@ -602,6 +610,7 @@ tasklist_([E1|T1], [E2|T2], [E3|T3], [E4|T4], [E5|T5], [E6|T6], [E7|T7], G) :-
 :- meta_predicate(tasklist(5, ?, ?, ?, ?, ?)).
 :- meta_predicate(tasklist(6, ?, ?, ?, ?, ?, ?)).
 :- meta_predicate(tasklist(7, ?, ?, ?, ?, ?, ?, ?)).
+:- meta_predicate(tasklist(8, ?, ?, ?, ?, ?, ?, ?)).
 */
 
 :- help(tasklist(:callable,+list), [iso(false)]).
@@ -611,3 +620,4 @@ tasklist_([E1|T1], [E2|T2], [E3|T3], [E4|T4], [E5|T5], [E6|T6], [E7|T7], G) :-
 :- help(tasklist(:callable,+list,+list,+list,+list,+list), [iso(false)]).
 :- help(tasklist(:callable,+list,+list,+list,+list,+list,+list), [iso(false)]).
 :- help(tasklist(:callable,+list,+list,+list,+list,+list,+list,+list), [iso(false)]).
+:- help(tasklist(:callable,+list,+list,+list,+list,+list,+list,+list,+list), [iso(false)]).
