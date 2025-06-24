6e49c63a17ebc98b64cc051946d642c8b0dc863e
Andrew Davison <andrew.davison@gmail.com>
2025-06-24 16:43:21 +1000

    WIP

--- src/internal.h
+++ src/internal.h
@@ -924,26 +924,26 @@ inline static void unshare_cell_(cell *c)
 	if (is_strbuf(c)) {
 		if (--c->val_strb->refcnt == 0) {
 			free(c->val_strb);
-			c->flags = 0;
+			c->tag = TAG_EMPTY;
 		}
 	} else if (is_bigint(c)) {
 		if (--c->val_bigint->refcnt == 0)	{
 			mp_int_clear(&c->val_bigint->ival);
 			free(c->val_bigint);
-			c->flags = 0;
+			c->tag = TAG_EMPTY;
 		}
 	} else if (is_rational(c)) {
 		if (--c->val_bigint->refcnt == 0)	{
 			mp_rat_clear(&c->val_bigint->irat);
 			free(c->val_bigint);
-			c->flags = 0;
+			c->tag = TAG_EMPTY;
 		}
 	} else if (is_blob(c)) {
 		if (--c->val_blob->refcnt == 0) {
 			free(c->val_blob->ptr2);
 			free(c->val_blob->ptr);
 			free(c->val_blob);
-			c->flags = 0;
+			c->tag = TAG_EMPTY;
 		}
 	} else if (is_dbid(c)) {
 		if (--c->val_blob->refcnt == 0) {
@@ -952,7 +952,7 @@ inline static void unshare_cell_(cell *c)
 			do_erase(m, ref);
 			free(c->val_blob->ptr2);
 			free(c->val_blob);
-			c->flags = 0;
+			c->tag = TAG_EMPTY;
 		}
 	} else if (is_kvid(c)) {
 		if (--c->val_blob->refcnt == 0) {
@@ -961,7 +961,7 @@ inline static void unshare_cell_(cell *c)
 			sl_del(m->pl->keyval, ref);
 			free(c->val_blob->ptr2);
 			free(c->val_blob);
-			c->flags = 0;
+			c->tag = TAG_EMPTY;
 		}
 	}
 }
