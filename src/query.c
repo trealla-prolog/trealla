44f475e06558f0d63188a6b63b81507f08620a69
Andrew Davison <andrew.davison@gmail.com>
2025-06-26 15:33:52 +1000

    WIP

--- src/query.c
+++ src/query.c
@@ -643,7 +643,7 @@ static void push_frame(query *q)
 
 	// Avoid long chains of useless returns...
 
-	if (is_end(next_cell) && !next_cell->ret_instr) {
+	if (q->pl->opt && is_end(next_cell) && !next_cell->ret_instr) {
 		f->prev = curr_f->prev;
 		f->instr = curr_f->instr;
 	} else {
