32d1ee2039c3c033d0fa6f53ee73daa32837fe15
Andrew Davison <andrew.davison@gmail.com>
2025-07-07 15:32:52 +1000

    More double bar syntax, re issue #820

--- src/parser.c
+++ src/parser.c
@@ -3227,14 +3227,13 @@ bool get_token(parser *p, bool last_op, bool was_postfix)
 								if (any)
 									SB_putchar(p->token, ',');
 
-								if (isdigit(ch))
-									SB_putchar(p->token, '\'');
+								SB_putchar(p->token, '\'');
+
+								if ((ch == '\'') || (ch == '\\'))
+									SB_putchar(p->token, '\\');
 
 								SB_putchar(p->token, ch);
-
-								if (isdigit(ch))
-									SB_putchar(p->token, '\'');
-
+								SB_putchar(p->token, '\'');
 								any = true;
 							}
 
