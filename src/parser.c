d3dc1b2772cc9599164903b01c284836ac8760c7
Andrew Davison <andrew.davison@gmail.com>
2025-07-07 13:04:47 +1000

    WIP

--- src/parser.c
+++ src/parser.c
@@ -3211,7 +3211,7 @@ bool get_token(parser *p, bool last_op, bool was_postfix)
 						|| (ch == '(') || ch == ')'
 						|| (ch == '[') || ch == ']'
 						|| (ch == '{') || ch == '}'
-						|| (ch == '-') || ch == '+'
+						|| (ch == '-')
 						) {
 						src = (char*)src;
 						p->quote_char = 0;
