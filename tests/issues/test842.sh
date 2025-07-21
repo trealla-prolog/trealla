aa4d3d4143923fd5fa82d117adc825e072fa59de
Andrew Davison <andrew.davison@gmail.com>
2025-07-22 08:33:24 +1000

    WIP

---
+++ tests/issues/test842.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# set -e
+
+TPL=./tpl
+
+TMP=tmp.out
+TMPPL=tmp.pl
+
+trap "rm -f $TMPPL" EXIT
+
+cat >$TMPPL <<EOF
+vx(X) :- -X=X.
+EOF
+
+$TPL $TMPPL <<EOF >$TMP
+vx(Y), X = 1.
+halt.
+EOF
+
+rm $TMPPL
