14f090e8447aac5b9bf2a5b6b1f12b671473077b
Andrew Davison <andrew.davison@gmail.com>
2025-07-22 10:58:31 +1000

    Toplevel var names cyclic, re issue #842

--- src/print.c
+++ src/print.c
@@ -526,7 +526,10 @@ static bool dump_variable(query *q, cell *c, pl_idx c_ctx, bool running)
 		l_ctx = running ? q->latest_ctx : 0;
 	}
 
-	if (true && 0) {
+	c = deref(q, c, c_ctx);
+	c_ctx = q->latest_ctx;
+
+	if (q->do_dump_vars && is_cyclic_term(q, c, c_ctx)) {
 		SB_sprintf(q->sb, "%s", GET_POOL(q, q->top->vartab.off[q->dump_var_num]));
 		return true;
 	}
