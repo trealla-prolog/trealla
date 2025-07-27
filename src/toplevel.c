78ba99c6376f46ce108f4a934fbe1b0dfd49a576
Andrew Davison <andrew.davison@gmail.com>
2025-07-27 12:11:59 +1000

    Top-level set numbervars(true) for output, re issue #853

--- src/toplevel.c
+++ src/toplevel.c
@@ -472,6 +472,7 @@ void dump_vars(query *q, bool partial)
 		q->quoted = q->pl->def_quoted ? 1 : 0;
 		q->parens = parens;
 		q->dump_var_num = i;
+		q->numbervars = true;
 		e->vgen = ++q->vgen;
 
 		print_term(q, stdout, c, c_ctx, 1);
