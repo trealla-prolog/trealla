dff5abb5139c906e8b9e2d3c7b152193ede0bc68
Andrew Davison <andrew.davison@gmail.com>
2025-08-01 18:54:16 +1000

    WIP

--- src/terms.c
+++ src/terms.c
@@ -94,7 +94,7 @@ static void collect_vars_internal(query *q, cell *p1, pl_idx p1_ctx, unsigned de
 		return;
 	}
 
-	if (!is_compound(p1))
+	if (!is_compound(p1) || is_ground(p1))
 		return;
 
 	if (is_iso_list(p1)) {
@@ -329,7 +329,7 @@ static bool is_cyclic_term_lists(query *q, cell *p1, pl_idx p1_ctx, unsigned dep
 
 static bool is_cyclic_term_internal(query *q, cell *p1, pl_idx p1_ctx, unsigned depth)
 {
-	if (!is_compound(p1) /*|| is_ground(p1)*/)
+	if (!is_compound(p1) || is_ground(p1))
 		return false;
 
 	if (is_iso_list(p1))
