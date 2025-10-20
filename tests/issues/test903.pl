23e3edde08e23bd52ad8a67a37bc0ff76a2083e8
Andrew Davison <andrew.davison@gmail.com>
2025-10-21 07:14:33 +1000

    WIP

--- tests/issues/test903.pl
+++ tests/issues/test903.pl
@@ -2,5 +2,4 @@
 
 main :-
 	catch((number_chars(N,"0_%0"),fail), _, write(ok1)), nl,
-	catch((number_chars(N,"0x0_/*b"),fail), _, write(ok2)), nl,
-	catch((number_chars(N,"0x0_\n%1"),fail), _, write(ok3)), nl.
+	catch((number_chars(N,"0x0_/*b"),fail), _, write(ok2)), nl.
