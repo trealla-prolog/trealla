014942927de3266bd43cbb772590a8b4a084fd27
Andrew Davison <andrew.davison@gmail.com>
2025-10-21 07:31:19 +1000

    WIP

--- tests/issues/test903.pl
+++ tests/issues/test903.pl
@@ -1,5 +1,7 @@
 :- initialization(main).
 
 main :-
-	catch((number_chars(N,"0_%0"),fail), _, write(ok1)), nl,
-	catch((number_chars(N,"0x0_/*b"),fail), _, write(ok2)), nl.
+	catch((number_chars(_,"0_%0"),fail), _, write(ok1)), nl,
+	catch((number_chars(_,"0x0_/*b"),fail), _, write(ok2)), nl,
+	number_chars(N3,"0_\n123"), write(N3), nl,
+	number_chars(N4,"0x0_\n123"), write(N4), nl.
