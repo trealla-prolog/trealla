1fafba0ec2820ea39b5521b98c838f4b28395c79
Andrew Davison <andrew.davison@gmail.com>
2026-03-03 15:44:29 +1000

    WIP

---
+++ samples/db-stress.pl
@@ -0,0 +1,57 @@
+run :-
+	run1,
+	fail.
+run :-
+	run2,
+	fail.
+run :-
+	run3,
+	fail.
+run.
+
+run1 :-
+	writeln('run1...'),
+	between(1,10,I),
+		write(I), nl,
+		test1,
+		fail.
+run1.
+
+test1 :-
+	between(1,1000000,I),
+		assertz(f(I)),
+		fail.
+test1 :-
+	retractall(f(_)).
+
+run2 :-
+	writeln('run2...'),
+	between(1,10,I),
+		write(I), nl,
+		test2,
+		fail.
+run2.
+
+test2 :-
+	between(1,1000000,I),
+		assertz(f(I)),
+		fail.
+test2 :-
+	abolish(f/1).
+
+run3 :-
+	writeln('run3...'),
+	between(1,10,I),
+		write(I), nl,
+		test3,
+		fail.
+run3.
+
+test3 :-
+	between(1,1000000,I),
+		assertz(f(I)),
+		fail.
+test3 :-
+	retract(f(_)),
+	fail.
+test3.
