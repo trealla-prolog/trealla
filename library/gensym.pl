7f551b6d38d459651f55215d9792e42a8250559f
Andrew Davison <andrew.davison@gmail.com>
2026-04-05 09:02:27 +1000

    WIP

--- library/gensym.pl
+++ library/gensym.pl
@@ -1,23 +1,32 @@
-:- module(gensym, [gensym/2, reset_gensym/1]).
+:- module(gensym, [gensym/2,
+		   reset_gensym/1]).
 
 :- use_module(library(error)).
+:- use_module(library(lists)).
+:- use_module(library(iso_ext)).
 :- use_module(library(si)).
 
 gensym_key(Base, BaseKey) :-
-	atom_concat('gensym_', Base, BaseKey).
+    atom_concat('gensym_', Base, BaseKey).
+
+append_id(Base, UniqueID, Unique) :-
+    atom_chars(Base, BaseChars),
+    number_chars(UniqueID, IDChars),
+    append(BaseChars, IDChars, AtomChars),
+    atom_chars(Unique, AtomChars).
 
 gensym(Base, Unique) :-
-	must_be(var, Unique),
-	atom_si(Base),
-	gensym_key(Base, BaseKey),
-	( bb_get(BaseKey, UniqueID0)
-	-> N is UniqueID0 + 1
-	; N is 1
-	),
-	atomic_concat(Base, N, Unique),
-	bb_put(BaseKey, N).
+    must_be(var, Unique),
+    atom_si(Base),
+    gensym_key(Base, BaseKey),
+    (  bb_get(BaseKey, UniqueID0) -> true
+    ;  UniqueID0 = 0
+    ),
+    UniqueID is UniqueID0 + 1,
+    append_id(Base, UniqueID, Unique),
+    bb_put(BaseKey, UniqueID).
 
 reset_gensym(Base) :-
-	atom_si(Base),
-	gensym_key(Base, BaseKey),
-	bb_put(BaseKey, 0).
+    atom_si(Base),
+    gensym_key(Base, BaseKey),
+    bb_put(BaseKey, 0).
