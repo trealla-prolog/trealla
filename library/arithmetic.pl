367d1f4b1def0c0d4986cb9a3425b2a1e49bd321
Andrew Davison <andrew.davison@gmail.com>
2026-04-16 18:48:55 +1000

    Fixes for rationals

--- library/arithmetic.pl
+++ library/arithmetic.pl
@@ -28,36 +28,43 @@ popcount(I, N) :-
 
 :- help(popcount(+integer,?integer), [iso(false)]).
 
-/**
- * number_to_rational(X):
- * If X is a number then the function returns an approximate rational number.
- */
-% number_to_rational(+Number, -Rational)
 number_to_rational(F, R) :- F < 0, !,
    H is -F,
    number_to_rational(H, A rdiv B),
    C is -A,
    R is C rdiv B.
 number_to_rational(F, R) :-
-   rat_start2(F, V, W),
+   rat_start_(F, V, W),
    divmod(V, W, D, U),
-   rat_iter2(W rdiv U, D rdiv 1, 1 rdiv 0, F, R0),
+   rat_iter_(W rdiv U, D rdiv 1, 1 rdiv 0, F, R0),
    R is R0.
 
-% rat_start2(+Number, -Integer, -Integer)
-rat_start2(F, V, W) :-
-   parts(F, M, E),
+% rat_start(+Number, -Integer, -Integer)
+rat_start_(F, V, W) :-
+   parts_(F, M, E),
    (E < 0 ->
        V = M, W is 2^(-E);
        V is M*E^2, W = 1).
 
-% rat_iter(+Rational, +Rational, +Rational, +Number, -Rational)
-rat_iter2(_, X, _, Y, X) :- X =:= Y, !.
-rat_iter2(_ rdiv 0, X, _, _, X) :- !.
-rat_iter2(V rdiv W, M rdiv N, P rdiv Q, Y, X) :-
+rat_iter_(_, X, _, Y, X) :- X =:= Y, !.
+rat_iter_(_ rdiv 0, X, _, _, X) :- !.
+rat_iter_(V rdiv W, M rdiv N, P rdiv Q, Y, X) :-
    divmod(V, W, D, U),
    R is D*M+P,
    S is D*N+Q,
-   rat_iter2(W rdiv U, R rdiv S, M rdiv N, Y, X).
+   rat_iter_(W rdiv U, R rdiv S, M rdiv N, Y, X).
+
+parts_(F, M, E) :-
+   logb_(F, G),
+   E is G-52,
+   U is -E,
+   scalb_(F, U, N),
+   M is truncate(N).
+
+scalb_(M, E, R) :-
+   R is M*2**E.
+
+logb_(M, E) :-
+   E is floor(log(M)/log(2)).
 
 :- help(number_to_rational(+number,-rational), [iso(false)]).
