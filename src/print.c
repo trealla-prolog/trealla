6a8ab083d351b17262b2b17e548bdbb16c50c935
Andrew Davison <andrew.davison@gmail.com>
2026-05-06 08:03:05 +1000

    WIP

--- src/print.c
+++ src/print.c
@@ -331,7 +331,7 @@ size_t sprint_int(char *dst, size_t dstlen, pl_int n, int base)
 		else
 			dst++;
 
-		// NOTE: according to the man heap_pages:
+		// NOTE: according to the man pages:
 		//
 		//		"Trying to take the absolute value of
 		// 		the most negative integer is not defined."
