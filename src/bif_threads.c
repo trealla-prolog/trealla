b26686058e3cd3bee335bbafa719f82f25628604
Andrew Davison <andrew.davison@gmail.com>
2026-05-06 10:37:30 +1000

    Improve thread timeout latency

--- src/bif_threads.c
+++ src/bif_threads.c
@@ -472,7 +472,7 @@ static bool do_match_message(query *q, unsigned chan, bool is_peek, double timeo
 					return false;
 				}
 
-				suspend_thread(t, 1);
+				suspend_thread(t, 10);
 			}
 			 while (!list_count(&t->queue) && !list_count(&t->signals) && !q->halt && !q->abort && (cnt++ < 1000));
 
