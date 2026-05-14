948f46471c1054bbf39f96ced635bbcbb76e0ab2
Andrew Davison <andrew.davison@gmail.com>
2026-05-15 09:56:49 +1000

    Samples to test hreads

--- samples/threads.pl
+++ samples/threads.pl
@@ -1,10 +1,10 @@
 test1 :-
-	writeln('Test simple sender/receiver'),
+	writeln('Test simple sender/receiver with signal to abort'),
 	thread_create(test1_receiver,T1,[]),
-	thread_create(test2_sender(100000,T1),T2,[]),
+	thread_create(test1_sender(100000,T1),T2,[]),
 	thread_join(T2,S1),
 	thread_join(T1,S2),
-	writeln(done),
+	writeln(done(t1=S1,t2=S2)),
 	halt.
 
 test1_receiver :-
@@ -23,24 +23,24 @@ test1_sender(N,To) :-
 	test1_sender(M,To).
 
 test2 :-
-	writeln('Test simple sender/receiver'),
+	writeln('Test simple sender/receiver with shutdown message'),
 	thread_create(test2_receiver,T1,[]),
 	thread_create(test2_sender(100000,T1),T2,[]),
 	thread_join(T2,S1),
 	thread_join(T1,S2),
-	writeln(done),
+	writeln(done(t1=S1,t2=S2)),
 	halt.
 
 test2_receiver :-
 	thread_self(Me),
 	thread_get_message(Me,Msg),
 	writeln(got(Msg)),
-	test2_receiver.
+	(Msg == shutdown -> halt ; test2_receiver).
 
 test2_sender(0,To) :-
-	sleep(1),
 	writeln('Sending halt signal'),
-	thread_signal(To,halt).
+	thread_send_message(To,shutdown),
+	halt.
 test2_sender(N,To) :-
 	thread_send_message(To,N),
 	M is N-1,
