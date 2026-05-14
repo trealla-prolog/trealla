316129a0e578d50c6e53a996ae65da7f73e94917
Andrew Davison <andrew.davison@gmail.com>
2026-05-15 09:43:17 +1000

    Samples to test hreads

--- samples/threads.pl
+++ samples/threads.pl
@@ -1,23 +1,48 @@
 test1 :-
 	writeln('Test simple sender/receiver'),
-	thread_create(test_receiver,T1,[]),
-	thread_create(test_sender(1000,T1),T2,[]),
+	thread_create(test1_receiver,T1,[]),
+	thread_create(test2_sender(100000,T1),T2,[]),
 	thread_join(T2,S1),
 	thread_join(T1,S2),
 	writeln(done),
 	halt.
 
-test_receiver :-
+test1_receiver :-
 	thread_self(Me),
 	thread_get_message(Me,Msg),
 	writeln(got(Msg)),
-	test_receiver.
+	test1_receiver.
 
-test_sender(0,To) :-
+test1_sender(0,To) :-
+	sleep(1),
 	writeln('Sending halt signal'),
 	thread_signal(To,halt).
-test_sender(N,To) :-
+test1_sender(N,To) :-
 	thread_send_message(To,N),
 	M is N-1,
-	test_sender(M,To).
+	test1_sender(M,To).
+
+test2 :-
+	writeln('Test simple sender/receiver'),
+	thread_create(test2_receiver,T1,[]),
+	thread_create(test2_sender(100000,T1),T2,[]),
+	thread_join(T2,S1),
+	thread_join(T1,S2),
+	writeln(done),
+	halt.
+
+test2_receiver :-
+	thread_self(Me),
+	thread_get_message(Me,Msg),
+	writeln(got(Msg)),
+	test2_receiver.
+
+test2_sender(0,To) :-
+	sleep(1),
+	writeln('Sending halt signal'),
+	thread_signal(To,halt).
+test2_sender(N,To) :-
+	thread_send_message(To,N),
+	M is N-1,
+	test2_sender(M,To).
 
