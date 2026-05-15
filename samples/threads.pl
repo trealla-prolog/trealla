60eccca8182d71b1adfca95ca9eb273dfd64f842
Andrew Davison <andrew.davison@gmail.com>
2026-05-15 10:20:50 +1000

    Samples to test threads

--- samples/threads.pl
+++ samples/threads.pl
@@ -10,11 +10,11 @@ test1 :-
 test1_receiver :-
 	thread_self(Me),
 	thread_get_message(Me,Msg),
-	writeln(got(Msg)),
+	write(got(Msg)),write('    \r'),
 	test1_receiver.
 
 test1_sender(0,To) :-
-	sleep(3),
+	sleep(1),
 	writeln('Sending halt signal'),
 	thread_signal(To,halt).
 test1_sender(N,To) :-
@@ -25,7 +25,7 @@ test1_sender(N,To) :-
 test2 :-
 	writeln('Test simple sender/receiver with shutdown message'),
 	thread_create(test2_receiver,T1,[]),
-	thread_create(test2_sender(100_000,T1),T2,[]),
+	thread_create(test2_sender(1_000_000,T1),T2,[]),
 	thread_join(T2,S1),
 	thread_join(T1,S2),
 	writeln(done(t1=S1,t2=S2)),
@@ -34,8 +34,8 @@ test2 :-
 test2_receiver :-
 	thread_self(Me),
 	thread_get_message(Me,Msg),
-	writeln(got(Msg)),
-	(Msg == shutdown -> halt ; test2_receiver).
+	write(got(Msg)),write('\r'),
+	(Msg == shutdown -> (nl,halt) ; test2_receiver).
 
 test2_sender(0,To) :-
 	writeln('Sending halt signal'),
@@ -45,4 +45,33 @@ test2_sender(N,To) :-
 	thread_send_message(To,N),
 	M is N-1,
 	test2_sender(M,To).
+
+test3 :-
+	writeln('Test simple ping-pong with shutdown message'),
+	thread_create(test3_receiver,T1,[]),
+	thread_create(test3_sender(1_000_000,T1),T2,[]),
+	thread_join(T2,S1),
+	thread_join(T1,S2),
+	writeln(done(t1=S1,t2=S2)),
+	halt.
+
+test3_receiver :-
+	thread_self(Me),
+	thread_get_message(Me,Msg),
+	write(got(Msg)), write('\r'),
+	(Msg == (nl,shutdown) -> halt ; true),
+	Msg = msg(_I,From),
+	thread_send_message(From,Msg),
+	test3_receiver.
+
+test3_sender(0,To) :-
+	writeln('Sending halt signal'),
+	thread_send_message(To,shutdown),
+	halt.
+test3_sender(N,To) :-
+	thread_self(Me),
+	thread_send_message(To,msg(N,Me)),
+	thread_get_message(Me,msg(N,Me)),
+	M is N-1,
+	test3_sender(M,To).
 
