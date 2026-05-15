ac6f6d4c4931b5faa67bb0243cacca6af08d79e6
Andrew Davison <andrew.davison@gmail.com>
2026-05-15 10:31:42 +1000

    Samples to test threads

--- samples/threads.pl
+++ samples/threads.pl
@@ -75,3 +75,32 @@ test3_sender(N,To) :-
 	M is N-1,
 	test3_sender(M,To).
 
+test4 :-
+	writeln('Test as above but with at_exit(Goal)'),
+	thread_create(test4_receiver,T1,[at_exit(assertz(t(T1)))]),
+	thread_create(test4_sender(100_000,T1),T2,[at_exit(assertz(t(T2)))]),
+	thread_join(T2,S1),
+	thread_join(T1,S2),
+	retract(t(T)),writeln(t(T)),fail;
+	(writeln(done(t1=S1,t2=S2)),halt).
+
+test4_receiver :-
+	thread_self(Me),
+	thread_get_message(Me,Msg),
+	write(got(Msg)), write('\r'),
+	(Msg == (nl,shutdown) -> halt ; true),
+	Msg = msg(_I,From),
+	thread_send_message(From,Msg),
+	test4_receiver.
+
+test4_sender(0,To) :-
+	writeln('Sending halt signal'),
+	thread_send_message(To,shutdown),
+	halt.
+test4_sender(N,To) :-
+	thread_self(Me),
+	thread_send_message(To,msg(N,Me)),
+	thread_get_message(Me,msg(N,Me)),
+	M is N-1,
+	test4_sender(M,To).
+
