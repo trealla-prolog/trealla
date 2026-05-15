98a9a27e3876e1d72b514801e465e2fecdbbaf3a
Andrew Davison <andrew.davison@gmail.com>
2026-05-15 10:05:27 +1000

    Samples to test hreads

--- samples/threads.pl
+++ samples/threads.pl
@@ -1,21 +1,21 @@
 test1 :-
-	write('Test simple sender/receiver with signal to abort'), nl,
+	writeln('Test simple sender/receiver with signal to abort'),
 	thread_create(test1_receiver,T1,[]),
-	thread_create(test1_sender(100000,T1),T2,[]),
+	thread_create(test1_sender(100_000,T1),T2,[]),
 	thread_join(T2,S1),
 	thread_join(T1,S2),
-	write(done(t1=S1,t2=S2)), nl,
+	writeln(done(t1=S1,t2=S2)),
 	halt.
 
 test1_receiver :-
 	thread_self(Me),
 	thread_get_message(Me,Msg),
-	write(got(Msg)), nl,
+	writeln(got(Msg)),
 	test1_receiver.
 
 test1_sender(0,To) :-
-	sleep(1),
-	write('Sending halt signal'), nl,
+	sleep(3),
+	writeln('Sending halt signal'),
 	thread_signal(To,halt).
 test1_sender(N,To) :-
 	thread_send_message(To,N),
@@ -23,22 +23,22 @@ test1_sender(N,To) :-
 	test1_sender(M,To).
 
 test2 :-
-	write('Test simple sender/receiver with shutdown message'),  nl,
+	writeln('Test simple sender/receiver with shutdown message'),
 	thread_create(test2_receiver,T1,[]),
-	thread_create(test2_sender(100000,T1),T2,[]),
+	thread_create(test2_sender(100_000,T1),T2,[]),
 	thread_join(T2,S1),
 	thread_join(T1,S2),
-	write(done(t1=S1,t2=S2)), nl,
+	writeln(done(t1=S1,t2=S2)),
 	halt.
 
 test2_receiver :-
 	thread_self(Me),
 	thread_get_message(Me,Msg),
-	write(got(Msg)), nl,
+	writeln(got(Msg)),
 	(Msg == shutdown -> halt ; test2_receiver).
 
 test2_sender(0,To) :-
-	write('Sending halt signal'), nl,
+	writeln('Sending halt signal'),
 	thread_send_message(To,shutdown),
 	halt.
 test2_sender(N,To) :-
