8d9b665f67965c447a5ee5322c0716d6bc491ab8
Andrew Davison <andrew.davison@gmail.com>
2026-05-19 17:39:50 +1000

    Try this, re issue #1026

--- src/bif_predicates.c
+++ src/bif_predicates.c
@@ -58,7 +58,8 @@ static bool bif_findnsols_ge_2(query *q)
 	if (is_integer(p2)) {
 		count = get_smallint(p2);
 	} else {
-		count = get_smallint(p2+1);
+		p2 = deref(q, p2+1, p2_ctx);
+		count = get_smallint(p2);
 	}
 
 	if (num < count) {
@@ -125,14 +126,15 @@ static bool bif_findnsols_4(query *q)
 			make_int(tmp+num_cells++, 1);
 			make_int(tmp+num_cells++, nsols);
 		} else {
-			tmp = prepare_call(q, CALL_NOSKIP, tmp2, p2_ctx, 1+p1->num_cells+3+3+1);
+			tmp = prepare_call(q, CALL_NOSKIP, tmp2, p2_ctx, 1+p1->num_cells+3+2+1);
 			CHECKED(tmp, drop_queuen(q));
 			num_cells = tmp2->num_cells;
 			make_instr(tmp+num_cells++, g_sys_queue_s, bif_sys_queue_1, 1, p1->num_cells);
 			num_cells += dup_cells_by_ref(tmp+num_cells, p1, p1_ctx, p1->num_cells);
-			make_instr(tmp+num_cells++, g_ge_s, bif_findnsols_ge_2, 2, 3);
+			make_instr(tmp+num_cells++, g_ge_s, bif_findnsols_ge_2, 2, 2);
 			make_int(tmp+num_cells++, 1);
-			num_cells += dup_cells_by_ref(tmp+num_cells, p0, p0_ctx, p0->num_cells);
+			cell *p01 = p0 + 1;
+			make_ref(tmp+num_cells++, p01->var_num, p0_ctx);
 		}
 
 		make_instr(tmp+num_cells++, g_sys_list_s, bif_sys_list_1, 1, 1);
