655f77d84d95986ddff5402a8bc7d111c64747c4
Andrew Davison <andrew.davison@gmail.com>
2026-05-30 14:06:04 +1000

    WIP

--- library/builtins.pl
+++ library/builtins.pl
@@ -94,13 +94,7 @@ raw_argv(L) :- current_prolog_flag(raw_argv, L).
 	'$undo_trail'(Vars, State),
 	process_vars_(Vars, [], Goals),
 	'$redo_trail'(State),
-	xmaplist_(Goals).
-
-xmaplist_([]).
-xmaplist_([E|T]) :-
-	once(E),
-	xmaplist_(T).
-
+	maplist(once, Goals).
 
 process_vars_([], Goals, Goals).
 process_vars_([Var-Val|Vars], SoFar, Goals) :-
