e07b35436704652945ff288050b617ec478b91b3
Andrew Davison <andrew.davison@gmail.com>
2026-06-18 12:07:33 +1000

    WIP

--- samples/test_timeout.pl
+++ samples/test_timeout.pl
@@ -1,15 +1,13 @@
 :- initialization(main).
 
 main :-
-	thread_create(catch(call_with_time_limit(1.0, run1), _, true), T1, []),
-	thread_create(catch(call_with_time_limit(2.0, run2), _, true), T2, []),
+	thread_create(catch(call_with_time_limit(1.0, run(here1)), _, true), T1, []),
+	thread_create(catch(call_with_time_limit(2.0, run(here2)), _, true), T2, []),
 	thread_join(T1),
 	writeln('\tdone1'),
 	thread_join(T2),
 	writeln('\tdone2').
 
-run1 :-
-	repeat, writeln(here1), sleep(0.25), fail.
+run(Msg) :-
+	repeat, writeln(Msg), sleep(0.25), fail.
 
-run2 :-
-	repeat, writeln(here2), sleep(0.25), fail.
