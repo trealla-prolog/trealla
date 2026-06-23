ae8d1b93684d17c568e9bb2c9f65c57ab930c58f
Andrew Davison <andrew.davison@gmail.com>
2026-06-23 10:43:13 +1000

    WIP

--- samples/test_timeout.pl
+++ samples/test_timeout.pl
@@ -20,9 +20,13 @@ run2(Msg) :-
 	repeat, writeln(Msg), sleep(0.25), fail.
 
 main3 :-
-	thread_create(catch(call_with_time_limit(1.0, run1), _, writeln(catch1)), T1, []),
-	thread_create(catch(call_with_time_limit(2.0, run1), _, writeln(catch2)), T2, []),
+	thread_create(catch(call_with_time_limit(1.0, run3), _, writeln(catch1)), T1, []),
+	thread_create(catch(call_with_time_limit(2.0, run3), _, writeln(catch2)), T2, []),
 	thread_join(T1),
 	writeln('done1'),
 	thread_join(T2),
 	writeln('done2').
+
+run3 :-
+	get_char(_).
+
