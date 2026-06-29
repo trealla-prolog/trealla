0ae1168c58651cdf798ccc9268f81cbf61842a6d
Andrew Davison <andrew.davison@gmail.com>
2026-06-30 09:29:36 +1000

    WIP

--- tests/run_valgrind.sh
+++ tests/run_valgrind.sh
@@ -12,7 +12,7 @@ trap "rm -f $TMP" EXIT
 failed_count=0
 succeeded_count=0
 
-for source in tests/tests/* tests/issues*/* tests/slow/*
+for source in tests/tests/* tests/issues*/* tests/slow/* tests/misc/*
 
 do
 	case "$source" in
