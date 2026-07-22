6679e07407a3f3e02adac50c8526c0a94b0679a4
andrew <andrew.davison@gmail.com>
2026-07-22 14:49:00 +1000

    WIP

--- src/prolog.c
+++ src/prolog.c
@@ -831,7 +831,7 @@ prolog *pl_create()
 
 	// Load some common libraries...
 
-	const char *bootstrap[] = {"builtins", "dcgs", NULL};
+	const char *bootstrap[] = {"builtins", NULL};
 
 	for (int i = 0; bootstrap[i]; i++) {
 		bool found = false;
