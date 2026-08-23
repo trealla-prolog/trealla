c437dc834a61e38beed9673445b37fa3a4cddf3b
andrew <andrew.davison@gmail.com>
2026-08-23 20:23:04 +1000

    Gustto phase 2

--- src/bif_predicates.c
+++ src/bif_predicates.c
@@ -2568,12 +2568,6 @@ static bool bif_sys_current_prolog_flag_2(query *q)
 		cell tmp;
 		make_atom(&tmp, g_true_s);
 		return unify(q, p2, p2_ctx, &tmp, q->st.cur_ctx);
-	} else if (!CMP_STRING_TO_CSTR(q, p1, "max_threads")) {
-		// We impose no limit of our own any more, so the ceiling is
-		// whatever the O/S will hand out.
-		cell tmp;
-		make_int(&tmp, g_max_os_threads ? g_max_os_threads : MAX_ACTUAL_THREADS);
-		return unify(q, p2, p2_ctx, &tmp, q->st.cur_ctx);
 	} else if (!CMP_STRING_TO_CSTR(q, p1, "os_threads")) {
 		cell tmp;
 		make_int(&tmp, g_max_os_threads ? g_max_os_threads : MAX_ACTUAL_THREADS);
@@ -2936,7 +2930,6 @@ static bool bif_iso_set_prolog_flag_2(query *q)
 		|| !CMP_STRING_TO_CSTR(q, p1, "threads")
 #if USE_THREADS
 		|| !CMP_STRING_TO_CSTR(q, p1, "os_threads")
-		|| !CMP_STRING_TO_CSTR(q, p1, "max_threads")
 #endif
 		|| !CMP_STRING_TO_CSTR(q, p1, "verbose")
 		|| !CMP_STRING_TO_CSTR(q, p1, "integer_rounding_function")
@@ -6845,7 +6838,6 @@ static void load_flags(query *q)
 #if USE_THREADS
 	SB_sprintf(pr, "'$current_prolog_flag'(%s, %s).\n", "threads", "true");
 	SB_sprintf(pr, "'$current_prolog_flag'(%s, %u).\n", "os_threads", g_max_os_threads ? g_max_os_threads : MAX_ACTUAL_THREADS);
-	SB_sprintf(pr, "'$current_prolog_flag'(%s, %u).\n", "max_threads", g_max_os_threads ? g_max_os_threads : MAX_ACTUAL_THREADS);
 #else
 	SB_sprintf(pr, "'$current_prolog_flag'(%s, %s).\n", "threads", "false");
 #endif
